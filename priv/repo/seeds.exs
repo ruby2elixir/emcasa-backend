50e55c9de40f0401365a6f3006e8c5b46d7fc94d
Gustavo Saiani <gustavo@poe.ma>
2017-06-30 16:19:22 +0200

    A basic Listing seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,17 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Re.{Repo, Listing}
+
+Repo.delete_all(Listing)
+
+Repo.insert! %Listing{
+  name: "Primeiro Apartamento",
+  description: "Descrição maravilhosa do primeiro apartamento"
+}
+
+Repo.insert! %Listing{
+  name: "Segundo Apartamento",
+  description: "Descrição maravilhosa do segundo apartamento"
+}
