b79f10bd8fef59196cbfd3e280e904ce9f055697
Gustavo Saiani <gustavo@poe.ma>
2017-08-02 17:01:27 +0200

    Remove cache_manifest from config prod

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,6 @@ config :re, ReWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "quiet-plateau-93105.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
