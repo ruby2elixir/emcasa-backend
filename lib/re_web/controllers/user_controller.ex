da60ca4ec404650e85adcfc66278b3fb1263ca7a
Gustavo Saiani <gustavo@poe.ma>
2017-10-01 17:08:25 -0300

    Add action receiving webservice post

--- lib/re_web/controllers/user_controller.ex
+++ lib/re_web/controllers/user_controller.ex
@@ -8,6 +8,22 @@ defmodule ReWeb.UserController do
     render(conn, "index.json", users: users)
   end
 
+  def create(conn, %{"user" => user_params, "listing" => listing_params}) do
+    changeset = User.changeset(%User{}, user_params)
+
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_status(:created)
+        |> put_resp_header("location", user_path(conn, :show, user))
+        |> render("show.json", user: user)
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(ReWeb.ChangesetView, "error.json", changeset: changeset)
+    end
+  end
+
   def create(conn, %{"user" => user_params}) do
     changeset = User.changeset(%User{}, user_params)
 
