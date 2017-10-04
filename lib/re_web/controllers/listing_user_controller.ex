e251d0174af0271ec85a458adcc48b2c33aada8d
Gustavo Saiani <gustavo@poe.ma>
2017-10-04 11:21:07 -0300

    Send sanity email on user upsert

--- lib/re_web/controllers/listing_user_controller.ex
+++ lib/re_web/controllers/listing_user_controller.ex
@@ -8,10 +8,18 @@ defmodule ReWeb.ListingUserController do
 
     case Repo.insert(changeset, on_conflict: :replace_all, conflict_target: :email) do
       {:ok, user} ->
+        SendGrid.Email.build()
+        |> SendGrid.Email.add_to("gustavo.saiani@emcasa.com")
+        |> SendGrid.Email.put_from("gustavo.saiani@emcasa.com")
+        |> SendGrid.Email.put_subject("Hello from Elixir")
+        |> SendGrid.Email.put_text("Sent with Elixir")
+        |> SendGrid.Mailer.send()
+
         conn
         |> put_status(:created)
         |> put_resp_header("location", user_path(conn, :show, user))
         |> render("show.json", user: user)
+
       {:error, changeset} ->
         conn
         |> put_status(:unprocessable_entity)
--- lib/re_web/models/listing_user.ex
+++ lib/re_web/models/listing_user.ex
@@ -7,4 +7,12 @@ defmodule ReWeb.ListingUser do
     belongs_to :listing, ReWeb.Listing
     belongs_to :user, ReWeb.User
   end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:listing_id, :user_id])
+  end
 end
