9581814ca79f40b2146f6b1f5ca33a5468d51563
Rodrigo Nonose <rodrigo.nonose@digitalnatives.hu>
2017-12-13 14:07:52 -0200

    Fix create/update tests.

--- test/re_web/controllers/listing_controller_test.exs
+++ test/re_web/controllers/listing_controller_test.exs
@@ -4,7 +4,7 @@ defmodule ReWeb.ListingControllerTest do
   alias ReWeb.Listing
   import Re.Factory
 
-  # @valid_attrs %{description: "some content", name: "some content", price: 1_000_000, rooms: 4, area: 140, garage_spots: 3,}
+  @valid_attrs %{type: "apto", score: 3, floor: "H1", complement: "basement", bathrooms: 2, description: "some content", price: 1_000_000, rooms: 4, area: 140, garage_spots: 3,}
   @valid_address_attrs %{street: "A Street", street_number: "100", neighborhood: "A Neighborhood", city: "A City", state: "ST", postal_code: "12345-678", lat: "25", lng: "25"}
   @invalid_attrs %{}
 
@@ -16,7 +16,10 @@ defmodule ReWeb.ListingControllerTest do
   setup %{conn: conn} do
     user = insert(:user)
     {:ok, jwt, _full_claims} = Guardian.encode_and_sign(user)
-    {:ok, conn: put_req_header(conn, "accept", "application/json"), jwt: jwt}
+    conn = conn
+      |> put_req_header("accept", "application/json")
+      |> put_req_header("authorization", "Token #{jwt}")
+    {:ok, conn: conn}
   end
 
   test "lists all entries on index", %{conn: conn} do
@@ -66,37 +69,36 @@ defmodule ReWeb.ListingControllerTest do
     end
   end
 
-  # test "creates and renders resource when data is valid", %{conn: conn} do
-  #   conn = post conn, listing_path(conn, :create), listing: @valid_attrs
-  #   assert json_response(conn, 201)["data"]["id"]
-  #   assert Repo.get_by(Listing, @valid_attrs)
-  # end
+  test "creates and renders resource when data is valid", %{conn: conn} do
+    conn = post conn, listing_path(conn, :create), listing: @valid_attrs, address: @valid_address_attrs
+    response = json_response(conn, 201)
+    assert response["listing"]["id"]
+    assert Repo.get_by(Listing, @valid_attrs)
+  end
 
-  test "does not create resource and renders errors when data is invalid", %{conn: conn, jwt: jwt} do
-    conn = conn |> put_req_header("authorization", "Token #{jwt}")
+  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
     conn = post(conn, listing_path(conn, :create), %{listing: @invalid_attrs, address: @valid_address_attrs})
     assert json_response(conn, 422)["errors"] != %{}
   end
 
-  # test "updates and renders chosen resource when data is valid", %{conn: conn} do
-  #   listing = Repo.insert! %Listing{}
-  #   conn = put conn, listing_path(conn, :update, listing), listing: @valid_attrs
-  #   assert json_response(conn, 200)["data"]["id"]
-  #   assert Repo.get_by(Listing, @valid_attrs)
-  # end
+  test "updates and renders chosen resource when data is valid", %{conn: conn} do
+    listing = insert(:listing, address: build(:address))
+    conn = put conn, listing_path(conn, :update, listing),
+      id: listing.id, listing: @valid_attrs, address: @valid_address_attrs
+    assert json_response(conn, 200)["listing"]["id"]
+    assert Repo.get_by(Listing, @valid_attrs)
+  end
 
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn, jwt: jwt} do
+  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
     address = Repo.insert! %ReWeb.Address{}
     listing = Repo.insert! %Listing{address_id: address.id}
 
-    conn = conn |> put_req_header("authorization", "Token #{jwt}")
     conn = put(conn, listing_path(conn, :update, listing), %{id: listing.id, listing: @invalid_attrs, address: @valid_address_attrs})
     assert json_response(conn, 422)["errors"] != %{}
   end
 
-  test "deletes chosen resource", %{conn: conn, jwt: jwt} do
+  test "deletes chosen resource", %{conn: conn} do
     listing = insert(:listing)
-    conn = conn |> put_req_header("authorization", "Token #{jwt}")
     conn = delete conn, listing_path(conn, :delete, listing)
     assert response(conn, 204)
     refute Repo.get(Listing, listing.id)
