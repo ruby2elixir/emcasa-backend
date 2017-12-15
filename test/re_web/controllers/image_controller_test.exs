28e8304c07aefcbce37881cf57d05f370ad6b536
Rodrigo Nonose <rodrigo.nonose@digitalnatives.hu>
2017-12-15 13:41:37 -0200

    Add test for unauthorized request.

--- test/re_web/controllers/image_controller_test.exs
+++ test/re_web/controllers/image_controller_test.exs
@@ -9,11 +9,12 @@ defmodule ReWeb.ListingImageControllerTest do
     conn =
       conn
       |> put_req_header("accept", "application/json")
-      |> put_req_header("authorization", "Token #{jwt}")
-    {:ok, conn: conn}
+
+    authenticated_conn = put_req_header(conn, "authorization", "Token #{jwt}")
+    {:ok, authenticated_conn: authenticated_conn, unauthenticated_conn: conn}
   end
 
-  test "lists all images for a listing", %{conn: conn} do
+  test "lists all images for a listing", %{authenticated_conn: conn} do
     address = insert(:address)
     image = insert(:image)
     listing = insert(:listing, images: [image], address: address)
@@ -26,5 +27,14 @@ defmodule ReWeb.ListingImageControllerTest do
         "position" => image.position
       }
     ]
+  end
+
+  test "don't list images for unauthenticated requests", %{unauthenticated_conn: conn} do
+    address = insert(:address)
+    image = insert(:image)
+    listing = insert(:listing, images: [image], address: address)
+
+    conn = get conn, listing_image_path(conn, :index, listing)
+    json_response(conn, 403)
   end
 end
