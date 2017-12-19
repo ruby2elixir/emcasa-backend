f9854605cac55e9d7471287c442d6d58ddc0b639
Rodrigo Nonose <rodrigo.nonose@digitalnatives.hu>
2017-12-19 12:08:36 -0200

    Sort images by position.

--- lib/re/images.ex
+++ lib/re/images.ex
@@ -6,22 +6,18 @@ defmodule Re.Images do
   import Ecto.Query
 
   alias Re.{
-    Listing,
     Image,
     Repo
   }
 
-  @query from l in Listing, preload: :images
-
   def all(%{"listing_id" => listing_id}) do
-    @query
-    |> Repo.get(listing_id)
-    |> Map.get(:images)
-    |> format_output()
+    q = from i in Image,
+      where: i.listing_id == ^listing_id,
+      order_by: [asc: i.position]
+
+    {:ok, Repo.all(q)}
   end
   def all(_), do: {:error, :bad_request}
-
-  defp format_output(images), do: {:ok, images}
 
   def update_per_listing(_listing, images_param) do
     Enum.each(images_param, &update_image/1)
