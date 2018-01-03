627eb2b56f14a1408facf5707536e0a5e631284a
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-01-03 15:53:27 -0200

    Remove unnecessary header.

--- lib/re_web/controllers/listing_user_controller.ex
+++ lib/re_web/controllers/listing_user_controller.ex
@@ -19,7 +19,6 @@ defmodule ReWeb.ListingUserController do
 
         conn
         |> put_status(:created)
-        |> put_resp_header("location", user_path(conn, :show, user))
         |> render("show.json", user: user)
     end
   end
