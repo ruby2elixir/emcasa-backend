7add54e566f3374a04b683ec9869dfaea918fca5
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-01-04 15:39:31 -0200

    Add ecto schema for filtering.

---
+++ lib/re/listings/filter.ex
@@ -0,0 +1,62 @@
+defmodule Re.Listings.Filter do
+  @moduledoc """
+  Module for grouping filter queries
+  """
+  use Ecto.Schema
+
+  import Ecto.{
+    Query,
+    Changeset
+  }
+
+  schema "listings_filter" do
+    field :max_price, :integer
+    field :min_price, :integer
+    field :rooms, :integer
+    field :min_area, :integer
+    field :max_area, :integer
+  end
+
+  @filters ~w(max_price min_price rooms min_area max_area)a
+
+  def changeset(struct, params \\ %{}), do: cast(struct, params, @filters)
+
+  def apply(query, params) do
+    filters = %__MODULE__{}
+      |> changeset(params)
+      |> Map.get(:changes)
+
+    query
+    |> max_price_filter(filters)
+    |> min_price_filter(filters)
+    |> rooms_filter(filters)
+    |> min_area_filter(filters)
+    |> max_area_filter(filters)
+  end
+
+  defp max_price_filter(query, %{max_price: max_price}) do
+    from l in query, where: l.price <= ^max_price
+  end
+  defp max_price_filter(query, _), do: query
+
+  defp min_price_filter(query, %{min_price: min_price}) do
+    from l in query, where: l.price >= ^min_price
+  end
+  defp min_price_filter(query, _), do: query
+
+  defp rooms_filter(query, %{rooms: rooms}) do
+    from l in query, where: l.rooms == ^rooms
+  end
+  defp rooms_filter(query, _), do: query
+
+  defp min_area_filter(query, %{min_area: min_area}) do
+    from l in query, where: l.area >= ^min_area
+  end
+  defp min_area_filter(query, _), do: query
+
+  defp max_area_filter(query, %{max_area: max_area}) do
+    from l in query, where: l.area <= ^max_area
+  end
+  defp max_area_filter(query, _), do: query
+
+end
---
+++ test/re/listings/listings_test.exs
@@ -0,0 +1,24 @@
+defmodule Re.ListingsTest do
+  use Re.ModelCase
+
+  alias Re.{
+    Listings
+  }
+
+  import Re.Factory
+
+  describe "all/1" do
+    test "should filter by max/min price" do
+      %{id: id1} = insert(:listing, price: 100, area: 40, rooms: 4, score: 4)
+      %{id: id2} = insert(:listing, price: 110, area: 60, rooms: 3,score: 3)
+      %{id: id3} = insert(:listing, price: 90, area: 50, rooms: 3,score: 2)
+
+      assert [%{id: ^id1}, %{id: ^id3}] = Listings.all(%{"max_price" => 105})
+      assert [%{id: ^id1}, %{id: ^id2}] = Listings.all(%{"min_price" => 95})
+      assert [%{id: ^id2}, %{id: ^id3}] = Listings.all(%{"rooms" => 3})
+      assert [%{id: ^id1}, %{id: ^id3}] = Listings.all(%{"max_area" => 55})
+      assert [%{id: ^id2}, %{id: ^id3}] = Listings.all(%{"min_area" => 45})
+    end
+  end
+
+end
