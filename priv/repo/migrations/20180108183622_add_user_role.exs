b3b56668e453928b3c75534987948a70f1b2db9a
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-01-09 16:28:55 -0200

    Set as admin only users with password.

--- priv/repo/migrations/20180108183622_add_user_role.exs
+++ priv/repo/migrations/20180108183622_add_user_role.exs
@@ -1,13 +1,20 @@
 defmodule Re.Repo.Migrations.AddUserRole do
   use Ecto.Migration
+  import Ecto.Query
+  alias Re.{
+    Repo,
+    User
+  }
+
+  @users_with_password from u in User, where: not is_nil(u.password)
 
   def up do
     alter table(:users) do
       add :role, :string
     end
-    flush
-    Re.Repo.update_all("users", set: [role: "admin"])
-    flush
+    flush()
+    @users_with_password
+    |> Repo.update_all(set: [role: "admin"])
   end
 
   def down do
