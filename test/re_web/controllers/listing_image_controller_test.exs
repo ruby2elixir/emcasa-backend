57d28e3a5f1483dcc92b693a6c4993ccb17be468
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-01-09 11:57:40 -0200

    Fix more warnings.

--- test/re_web/controllers/listing_image_controller_test.exs
+++ test/re_web/controllers/listing_image_controller_test.exs
@@ -11,9 +11,7 @@ defmodule ReWeb.ListingImageControllerTest do
   setup %{conn: conn} do
     user = insert(:user)
     {:ok, jwt, _full_claims} = Guardian.encode_and_sign(user)
-    conn =
-      conn
-      |> put_req_header("accept", "application/json")
+    conn = put_req_header(conn, "accept", "application/json")
 
     authenticated_conn = put_req_header(conn, "authorization", "Token #{jwt}")
     {:ok, authenticated_conn: authenticated_conn, unauthenticated_conn: conn}
--- test/re_web/controllers/neighborhood_controller_test.exs
+++ test/re_web/controllers/neighborhood_controller_test.exs
@@ -8,9 +8,7 @@ defmodule ReWeb.NeighborhoodControllerTest do
   setup %{conn: conn} do
     user = insert(:user)
     {:ok, jwt, _full_claims} = Guardian.encode_and_sign(user)
-    conn =
-      conn
-      |> put_req_header("accept", "application/json")
+    conn = put_req_header(conn, "accept", "application/json")
 
     authenticated_conn = put_req_header(conn, "authorization", "Token #{jwt}")
     {:ok, authenticated_conn: authenticated_conn, unauthenticated_conn: conn}
