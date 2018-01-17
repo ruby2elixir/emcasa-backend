2134f5cfe0742b617f7d76aa9f7ab8c2a598e7b8
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-01-17 11:54:26 -0200

    Remove commented route.

--- lib/re_web/router.ex
+++ lib/re_web/router.ex
@@ -34,7 +34,6 @@ defmodule ReWeb.Router do
       resources "/images", ImageController, only: [:index, :create, :delete]
     end
     put "/listings/:listing_id/image_order", ListingController, :order
-    # resources "/users", UserController, except: [:new, :edit, :create]
   end
 
   if Mix.env == :dev do
