3aaeb01867ce4ed0022862b6e29a44ab38bcad52
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-01-22 13:38:36 -0200

    Reorganize routes.

--- lib/re_web/router.ex
+++ lib/re_web/router.ex
@@ -1,14 +1,6 @@
 defmodule ReWeb.Router do
   use ReWeb, :router
 
-  pipeline :browser do
-    plug(:accepts, ["html"])
-    plug(:fetch_session)
-    plug(:fetch_flash)
-    plug(:protect_from_forgery)
-    plug(:put_secure_browser_headers)
-  end
-
   pipeline :public_api do
     plug(:accepts, ["json"])
   end
@@ -24,11 +16,17 @@ defmodule ReWeb.Router do
     resources("/neighborhoods", NeighborhoodController, only: [:index])
     resources("/listings", ListingController, only: [:index, :show])
     resources("/interests", InterestController, only: [:create])
-    post("/users/login", AuthController, :login)
-    post("/users/register", AuthController, :register)
-    put("/users/confirm", AuthController, :confirm)
-    post("/users/reset_password", AuthController, :reset_password)
-    post("/users/redefine_password", AuthController, :redefine_password)
+  end
+
+  scope "/users", ReWeb do
+    pipe_through(:public_api)
+
+    put("/confirm", AuthController, :confirm)
+
+    post("/login", AuthController, :login)
+    post("/register", AuthController, :register)
+    post("/reset_password", AuthController, :reset_password)
+    post("/redefine_password", AuthController, :redefine_password)
   end
 
   scope "/", ReWeb do
@@ -44,6 +42,14 @@ defmodule ReWeb.Router do
   end
 
   if Mix.env() == :dev do
+    pipeline :browser do
+      plug(:accepts, ["html"])
+      plug(:fetch_session)
+      plug(:fetch_flash)
+      plug(:protect_from_forgery)
+      plug(:put_secure_browser_headers)
+    end
+
     scope "/dev" do
       pipe_through(:browser)
 
