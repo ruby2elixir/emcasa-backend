04ea13a9b55e8c3068135b8792065f96f8041f65
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-02-15 15:38:32 -0200

    Add relaxation logic for area range.

--- lib/re/listings/filters/relax.ex
+++ lib/re/listings/filters/relax.ex
@@ -11,6 +11,13 @@ defmodule Re.Listings.Filters.Relax do
     |> min_price()
   end
 
+  def area(params) do
+    params
+    |> Filter.cast()
+    |> max_area()
+    |> min_area()
+  end
+
   defp max_price(%{max_price: max_price} = params) when is_not_nil(max_price) do
     Map.update(params, :max_price, 0, &(trunc(&1 * 1.1)))
   end
@@ -20,5 +27,15 @@ defmodule Re.Listings.Filters.Relax do
     Map.update(params, :min_price, 0, &(trunc(&1 * 0.9)))
   end
   defp min_price(params), do: params
+
+  defp max_area(%{max_area: max_area} = params) when is_not_nil(max_area) do
+    Map.update(params, :max_area, 0, &(trunc(&1 * 1.1)))
+  end
+  defp max_area(params), do: params
+
+  defp min_area(%{min_area: min_area} = params) when is_not_nil(min_area) do
+    Map.update(params, :min_area, 0, &(trunc(&1 * 0.9)))
+  end
+  defp min_area(params), do: params
 
 end
--- test/re/listings/filters/relax_test.exs
+++ test/re/listings/filters/relax_test.exs
@@ -24,4 +24,26 @@ defmodule Re.Listings.Filters.RelaxTest do
       assert %{min_price: 900_000} = Relax.price(%{min_price: 1_000_000, max_price: nil})
     end
   end
+
+  describe "area/1" do
+    test "max area" do
+      assert %{max_area: 55} = Relax.area(%{max_area: 50})
+    end
+
+    test "min area" do
+      assert %{min_area: 45} = Relax.area(%{min_area: 50})
+    end
+
+    test "min and max area" do
+      assert %{min_area: 45, max_area: 110} = Relax.area(%{min_area: 50, max_area: 100})
+    end
+
+    test "nil min area" do
+      assert %{max_area: 110} = Relax.area(%{min_area: nil, max_area: 100})
+    end
+
+    test "nil max area" do
+      assert %{min_area: 45} = Relax.area(%{min_area: 50, max_area: nil})
+    end
+  end
 end
