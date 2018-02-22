113e0577a7c576ef56e7fce1937dbb965c7aef36
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-02-22 10:59:12 -0300

    Only allow request from authenticated admins.

--- lib/re_web/controllers/listings/search_controller.ex
+++ lib/re_web/controllers/listings/search_controller.ex
@@ -1,22 +1,31 @@
 defmodule ReWeb.SearchController do
   use ReWeb, :controller
+  use ReWeb.GuardedController
 
   alias Re.Listings
 
   action_fallback(ReWeb.FallbackController)
 
-  def index(conn, _params) do
-    page = Listings.paginated()
+  plug(Guardian.Plug.EnsureAuthenticated)
 
-    render(
-      conn,
-      ReWeb.ListingView,
-      "paginated_index.json",
-      listings: page.entries,
-      page_number: page.page_number,
-      page_size: page.page_size,
-      total_pages: page.total_pages,
-      total_entries: page.total_entries
-    )
+  def index(conn, _params, user) do
+    with :ok <- authorize(user) do
+      page = Listings.paginated()
+
+      render(
+        conn,
+        ReWeb.ListingView,
+        "paginated_index.json",
+        listings: page.entries,
+        page_number: page.page_number,
+        page_size: page.page_size,
+        total_pages: page.total_pages,
+        total_entries: page.total_entries
+      )
+    end
   end
+
+  def authorize(%{role: "admin"}), do: :ok
+  def authorize(nil), do: {:error, :unauthorized}
+  def authorize(_), do: {:error, :forbidden}
 end
