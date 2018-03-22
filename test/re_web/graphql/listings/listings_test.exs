e0e67169e13fecee48701c52b3db53de47a6ed33
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-03-22 14:04:34 -0300

    Add graphql integration tests.

---
+++ test/re_web/graphql/listings/listings_test.exs
@@ -0,0 +1,74 @@
+defmodule ReWeb.GraphQLTest do
+  use ReWeb.ConnCase
+
+  import Re.Factory
+
+  alias Re.Listing
+  alias ReWeb.AbsintheHelpers
+
+  setup %{conn: conn} do
+    conn = put_req_header(conn, "accept", "application/json")
+    admin_user = insert(:user, email: "admin@email.com", role: "admin")
+    user_user = insert(:user, email: "user@email.com", role: "user")
+
+    {:ok,
+     unauthenticated_conn: conn,
+     admin_user: admin_user,
+     user_user: user_user,
+     admin_conn: login_as(conn, admin_user),
+     user_conn: login_as(conn, user_user)}
+  end
+
+  describe "mutations" do
+    test "admin should activate listing", %{admin_conn: conn} do
+      listing = insert(:listing, is_active: false)
+
+      mutation = """
+        mutation {
+          activateListing(id: #{listing.id}) {
+            id
+          }
+        }
+      """
+
+      post(conn, "/graphql_api", AbsintheHelpers.mutation_skeleton(mutation))
+
+      assert listing = Repo.get(Listing, listing.id)
+      assert listing.is_active
+    end
+
+    test "user should not activate listing", %{user_conn: conn} do
+      listing = insert(:listing, is_active: false)
+
+      mutation = """
+        mutation {
+          activateListing(id: #{listing.id}) {
+            id
+          }
+        }
+      """
+
+      post(conn, "/graphql_api", AbsintheHelpers.mutation_skeleton(mutation))
+
+      assert listing = Repo.get(Listing, listing.id)
+      refute listing.is_active
+    end
+
+    test "anonymous should not activate listing", %{unauthenticated_conn: conn} do
+      listing = insert(:listing, is_active: false)
+
+      mutation = """
+        mutation {
+          activateListing(id: #{listing.id}) {
+            id
+          }
+        }
+      """
+
+      post(conn, "/graphql_api", AbsintheHelpers.mutation_skeleton(mutation))
+
+      assert listing = Repo.get(Listing, listing.id)
+      refute listing.is_active
+    end
+  end
+end
