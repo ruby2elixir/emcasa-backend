dcfeb84a2a62785a96b828d695ba33a680e37755
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-04-25 16:28:56 -0300

    Add heroku configurations for elasticsearch

--- config/prod.exs
+++ config/prod.exs
@@ -47,39 +47,8 @@ config :re,
   from: System.get_env("ADMIN_EMAIL"),
   frontend_url: System.get_env("FRONTEND_URL")
 
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :re, ReWeb.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
-#
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
-#
-#     config :re, ReWeb.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
 
-# ## Using releases
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start the server for all endpoints:
-#
-#     config :phoenix, :serve_endpoints, true
-#
-# Alternatively, you can configure exactly which server to
-# start per endpoint:
-#
-#     config :re, ReWeb.Endpoint, server: true
-#
+config :re, ReWeb.Search.Cluster,
+  url: System.get_env("ELASTICSEARCH_URL"),
+  username: System.get_env("ELASTICSEARCH_KEY"),
+  password: System.get_env("ELASTICSEARCH_SECRET")
