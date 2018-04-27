4a6b8b27773ced16a536c353b3447e459d766dd6
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-04-27 13:26:34 -0300

    Reduce ciclomatic complexity

--- lib/re_web/search/document.ex
+++ lib/re_web/search/document.ex
@@ -11,17 +11,16 @@ defimpl Elasticsearch.Document, for: Re.Listing do
         #{listing.property_tax && "#{listing.property_tax} IPTU"}
         #{listing.maintenance_fee && "#{listing.maintenance_fee} condominio"}
         #{listing.area}
-        #{listing.rooms && if_not_zero(listing.rooms, "quarto", "quartos")}
-        #{listing.bathrooms && if_not_zero(listing.restrooms, "lavabo", "lavabos")}
-        #{listing.restrooms && if_not_zero(listing.restrooms, "lavabo", "lavabos")}
-        #{listing.garage_spots && if_not_zero(listing.garage_spots, "gargem", "garagens")}
-        #{listing.matterport_code}
-        #{listing.suites && if_not_zero(listing.suites, "suíte", "suítes")}
-        #{
-        listing.dependencies && if_not_zero(listing.dependencies, "dependência", "dependências")
-      }
-        #{listing.balconies && if_not_zero(listing.balconies, "varanda", "varandas")}
-        #{listing.has_elevator && "elevador"}
+        #{map_price(listing)}
+        #{map_property_tax(listing)}
+        #{map_maintenance_fee(listing)}
+        #{map_rooms(listing)}
+        #{map_bathrooms(listing)}
+        #{map_garage_spots(listing)}
+        #{map_restrooms(listing)}
+        #{map_suites(listing)}
+        #{map_dependencies(listing)}
+        #{map_balconies(listing)}
         #{listing.is_exclusive && "exclusivo"}
         #{listing.is_release && "lançamento"}
         #{listing.address.street}
@@ -31,6 +30,7 @@ defimpl Elasticsearch.Document, for: Re.Listing do
         #{listing.address.city}
         #{listing.address.state}
         #{listing.address.postal_code}
+        #{listing.matterport_code}
       """,
       inserted_at: listing.inserted_at,
       location: %{
@@ -40,7 +40,18 @@ defimpl Elasticsearch.Document, for: Re.Listing do
     }
   end
 
-  defp if_not_zero(0, _), do: ""
+  defp map_price(%{price: price}), do: price && "#{price} valor"
+  defp map_property_tax(%{property_tax: property_tax}), do: property_tax && "#{property_tax} IPTU"
+  defp map_maintenance_fee(%{maintenance_fee: maintenance_fee}), do: maintenance_fee && "#{maintenance_fee} condominio"
+  defp map_rooms(%{rooms: rooms}), do: if_not_zero(rooms, "quarto", "quartos")
+  defp map_bathrooms(%{bathrooms: bathrooms}), do: if_not_zero(bathrooms, "banheiro", "banheiros")
+  defp map_garage_spots(%{garage_spots: garage_spots}), do: if_not_zero(garage_spots, "garagem", "garagens")
+  defp map_restrooms(%{restrooms: restrooms}), do: if_not_zero(restrooms, "lavabo", "lavabos")
+  defp map_suites(%{suites: suites}), do: if_not_zero(suites, "suíte", "suítes")
+  defp map_dependencies(%{dependencies: dependencies}), do: if_not_zero(dependencies, "dependência", "dependências")
+  defp map_balconies(%{balconies: balconies}), do: if_not_zero(balconies, "varanda", "varandas")
+
+  defp if_not_zero(0, _, _), do: ""
   defp if_not_zero(1, singular, _plural), do: "1 #{singular}"
   defp if_not_zero(num, _singular, plural), do: "#{num} #{plural}"
 end
