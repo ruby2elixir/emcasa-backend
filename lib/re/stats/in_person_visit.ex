91cffa5d4409456f83389a1dd90179ef967fe6b3
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-05-10 14:41:41 -0300

    Add in-person visit schema

---
+++ lib/re/stats/in_person_visit.ex
@@ -0,0 +1,26 @@
+defmodule Re.Stats.InPersonVisit do
+  @moduledoc """
+  Model to record in person visits
+  """
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  schema "in_person_visits" do
+    field :date, :utc_datetime
+
+    belongs_to :listing, Re.Listing
+
+    timestamps()
+  end
+
+  @required ~w(date listing_id)a
+  @optional ~w()a
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, @required ++ @optional)
+    |> validate_required(@required)
+    |> foreign_key_constraint(:listing_id)
+  end
+end
