4061e72810cde0dbb661798ec18908bb8e110cf4
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-05-11 09:29:43 -0300

    Fix pipedrive url building

--- lib/re_web/integrations/pipedrive/client.ex
+++ lib/re_web/integrations/pipedrive/client.ex
@@ -1,4 +1,4 @@
-defmodule ReWeb.Integrations.Pipedrive do
+defmodule ReWeb.Integrations.Pipedrive.Client do
   @moduledoc """
   Module to wrap pipedrive.com API logic
   """
@@ -8,20 +8,21 @@ defmodule ReWeb.Integrations.Pipedrive do
   @token Application.get_env(:re, :pipedrive_token, "")
   @headers [{"Content-Type", "application/json"}]
 
+  def get(type \\ "", params \\ %{})
+
   def get(type, params) do
     params = Map.merge(params, %{api_token: @token})
 
     @url
-    |> build_uri(type, params)
+    |> build_uri(params, type)
     |> @http_client.get(@headers)
   end
 
   def build_uri(url, type, params) do
-    %URI{
-      host: url,
-      path: type,
-      port: 443,
-      query: URI.encode_query(params)
-    }
+    url
+    |> URI.parse()
+    |> URI.merge(type)
+    |> Map.put(:port, 443)
+    |> Map.put(:query, URI.encode_query(params))
   end
 end
