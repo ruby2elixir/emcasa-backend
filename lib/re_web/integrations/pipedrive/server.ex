1436050d3acced923e7174e02d91bd2fcc110e52
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-05-21 17:13:27 -0300

    Handle webhook and store in person visit

--- lib/re_web/integrations/pipedrive/server.ex
+++ lib/re_web/integrations/pipedrive/server.ex
@@ -6,6 +6,14 @@ defmodule ReWeb.Integrations.Pipedrive.Server do
 
   require Logger
 
+  alias Re.{
+    Repo,
+    Stats.InPersonVisit
+  }
+  alias ReWeb.Integrations.Pipedrive.Client
+
+  @attribute_key "226c74dbe45d5db69bbda19d6de53371db8a964a"
+
   @spec start_link :: GenServer.start_link()
   def start_link, do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
 
@@ -13,9 +21,39 @@ defmodule ReWeb.Integrations.Pipedrive.Server do
   def init(args), do: {:ok, args}
 
   @spec handle_cast({atom, map}, any) :: {:noreply, any}
-  def handle_cast({:handle_webhook, params}, state) do
+  def handle_cast({:handle_webhook, %{"deal_id" => deal_id, "marked_as_done_time" => done_time} = params}, state) do
     Logger.info("Webhook received with params #{inspect(params)}")
+
+    deal_id
+    |> get_listing_id()
+    |> save_visit(done_time)
 
     {:noreply, state}
   end
+
+  defp get_listing_id(deal_id) do
+    deal_id
+    |> fetch_deal()
+    |> IO.inspect
+    |> extract_listing_id()
+  end
+
+  defp fetch_deal(deal_id), do: Client.get("deals/#{deal_id}")
+
+  defp extract_listing_id({:ok, %{body: body}}) do
+    body
+    |> Poison.decode!()
+    |> Map.get("data")
+    |> Map.get(@attribute_key)
+  end
+
+  defp save_visit(listing_id, done_time) when is_integer(listing_id) do
+    %InPersonVisit{}
+    |> InPersonVisit.changeset(%{listing_id: listing_id, date: done_time})
+    |> Repo.insert()
+
+    Logger.info("In Person Visit recorded for listing #{listing_id} at #{done_time}")
+  end
+
+  defp save_visit(_, _), do: Logger.info("Listing ID is empty")
 end
