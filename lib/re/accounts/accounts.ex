9b0329679c2d5e74e9ceb624fc3c4b2c0e4dfb4c
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-06-13 13:55:00 -0300

    Remove unused module

--- lib/re/accounts/accounts.ex
+++ lib/re/accounts/accounts.ex
@@ -2,11 +2,8 @@ defmodule Re.Accounts do
   @moduledoc """
   Context boundary to Accounts management
   """
-  alias __MODULE__.DataloaderQueries
 
   def data(params), do: Dataloader.Ecto.new(Re.Repo, query: &query/2, default_params: params)
-
-  def query(User, args), do: DataloaderQueries.build(User, args)
 
   def query(query, _args), do: query
 end
