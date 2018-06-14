99e244ff94e673feb62b425d71dda0e3240ed9b2
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-06-14 11:41:50 -0300

    Handle nil rooms in related query

--- test/re/listings/related_test.exs
+++ test/re/listings/related_test.exs
@@ -40,5 +40,18 @@ defmodule Re.RelatedTest do
 
       assert %{listings: [%{id: ^id1}, %{id: ^id2}], remaining_count: 0} = Related.get(listing)
     end
+
+    test "should return a neighborhood and price match listing with nil rooms" do
+      listing =
+        insert(:listing, address: build(:address, neighborhood: "Copacabana"), price: 100_000, rooms: nil)
+
+      %{id: id2} =
+        insert(:listing, address: build(:address, neighborhood: "Copacabana"), price: 74_000)
+
+      %{id: id3} =
+        insert(:listing, address: build(:address, neighborhood: "Ipanema"), price: 76_000)
+
+      assert %{listings: [%{id: ^id2}, %{id: ^id3}], remaining_count: 0} = Related.get(listing)
+    end
   end
 end
