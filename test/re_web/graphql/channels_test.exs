7ebadf5042ce8fe197e73199bc91eba2c841551d
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-07-17 15:29:23 -0300

    Handle unauthenticated users

--- test/re_web/graphql/channels_test.exs
+++ test/re_web/graphql/channels_test.exs
@@ -68,7 +68,6 @@ defmodule ReWeb.GraphQL.ChannelsTest do
              ] == json_response(conn, 200)["data"]["userChannels"]
     end
 
-
     test "admin should list its channels", %{admin_conn: conn, admin_user: admin_user, user_user: user} do
       listing1 = insert(:listing)
       listing2 = insert(:listing)
@@ -128,6 +127,91 @@ defmodule ReWeb.GraphQL.ChannelsTest do
              ] == json_response(conn, 200)["data"]["userChannels"]
     end
   end
+
+    test "user should list its channels", %{user_conn: conn, admin_user: admin_user, user_user: user} do
+      listing1 = insert(:listing)
+      listing2 = insert(:listing)
+
+      {c1_id, m1c1_id, m2c1_id} = insert_channel_and_messages(listing1.id, user.id, admin_user.id)
+      {c2_id, m1c2_id, m2c2_id} = insert_channel_and_messages(listing2.id, user.id, admin_user.id)
+
+      query = """
+        {
+          userChannels {
+            id
+            participant1 { id }
+            participant2 { id }
+            listing { id }
+            unreadCount
+            messages { id }
+            lastMessage: messages (limit: 1) {
+              id
+            }
+            subsequentMessage: messages(limit: 1, offset: 1) {
+              id
+            }
+          }
+        }
+      """
+
+      conn =
+        post(conn, "/graphql_api", AbsintheHelpers.query_skeleton(query, "listingUserMessages"))
+
+      assert [
+               %{
+                 "id" => to_string(c2_id),
+                 "participant1" => %{"id" => to_string(admin_user.id)},
+                 "participant2" => %{"id" => to_string(user.id)},
+                 "listing" => %{"id" => to_string(listing2.id)},
+                 "unreadCount" => 1,
+                 "messages" => [
+                   %{"id" => to_string(m2c2_id)},
+                   %{"id" => to_string(m1c2_id)}
+                 ],
+                 "lastMessage" => [%{"id" => to_string(m2c2_id)}],
+                 "subsequentMessage" => [%{"id" => to_string(m1c2_id)}]
+               },
+               %{
+                 "id" => to_string(c1_id),
+                 "participant1" => %{"id" => to_string(admin_user.id)},
+                 "participant2" => %{"id" => to_string(user.id)},
+                 "listing" => %{"id" => to_string(listing1.id)},
+                 "unreadCount" => 1,
+                 "messages" => [
+                   %{"id" => to_string(m2c1_id)},
+                   %{"id" => to_string(m1c1_id)}
+                 ],
+                 "lastMessage" => [%{"id" => to_string(m2c1_id)}],
+                 "subsequentMessage" => [%{"id" => to_string(m1c1_id)}]
+               }
+             ] == json_response(conn, 200)["data"]["userChannels"]
+    end
+
+    test "anonymous should not list channels", %{unauthenticated_conn: conn} do
+      query = """
+        {
+          userChannels {
+            id
+            participant1 { id }
+            participant2 { id }
+            listing { id }
+            unreadCount
+            messages { id }
+            lastMessage: messages (limit: 1) {
+              id
+            }
+            subsequentMessage: messages(limit: 1, offset: 1) {
+              id
+            }
+          }
+        }
+      """
+
+      conn =
+        post(conn, "/graphql_api", AbsintheHelpers.query_skeleton(query, "listingUserMessages"))
+
+      assert %{"errors" => [%{"message" => "Unauthorized", "code" => 401}]} = json_response(conn, 200)
+    end
 
   defp insert_channel_and_messages(listing_id, user1_id, user2_id) do
     %{id: channel_id} =
