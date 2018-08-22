8a20036c94915203c723ed9f4bcae6aa6ed6c2bf
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-08-22 16:41:02 -0300

    Add schema type to interest type query

--- test/re_web/graphql/interests/interest_types/query_test.exs
+++ test/re_web/graphql/interests/interest_types/query_test.exs
@@ -16,71 +16,69 @@ defmodule ReWeb.GraphQL.InterestType.QueryTest do
      user_conn: login_as(conn, user_user)}
   end
 
-  describe "dashboard" do
-    test "should query interest types for admins", %{admin_conn: conn} do
-      insert(:interest_type, name: "type2", enabled: false)
+  test "should query interest types for admins", %{admin_conn: conn} do
+    insert(:interest_type, name: "type2", enabled: false)
 
-      query = """
-        {
-          interestTypes {
-            id
-            name
-          }
+    query = """
+      query InterestTypes {
+        interestTypes {
+          id
+          name
         }
-      """
+      }
+    """
 
-      conn = post(conn, "/graphql_api", AbsintheHelpers.query_skeleton(query, "dashboard"))
+    conn = post(conn, "/graphql_api", AbsintheHelpers.query_wrapper(query))
 
-      assert [
-               %{"id" => "1", "name" => "Me ligue dentro de 5 minutos"},
-               %{"id" => "2", "name" => "Me ligue em um horário específico"},
-               %{"id" => "3", "name" => "Agendamento por e-mail"},
-               %{"id" => "4", "name" => "Agendamento por Whatsapp"}
-             ] = json_response(conn, 200)["data"]["interestTypes"]
-    end
+    assert [
+             %{"id" => "1", "name" => "Me ligue dentro de 5 minutos"},
+             %{"id" => "2", "name" => "Me ligue em um horário específico"},
+             %{"id" => "3", "name" => "Agendamento por e-mail"},
+             %{"id" => "4", "name" => "Agendamento por Whatsapp"}
+           ] == json_response(conn, 200)["data"]["interestTypes"]
+  end
 
-    test "should query interest types for user", %{user_conn: conn} do
-      insert(:interest_type, name: "type2", enabled: false)
+  test "should query interest types for user", %{user_conn: conn} do
+    insert(:interest_type, name: "type2", enabled: false)
 
-      query = """
-        {
-          interestTypes {
-            id
-            name
-          }
+    query = """
+      query InterestTypes {
+        interestTypes {
+          id
+          name
         }
-      """
+      }
+    """
 
-      conn = post(conn, "/graphql_api", AbsintheHelpers.query_skeleton(query, "dashboard"))
+    conn = post(conn, "/graphql_api", AbsintheHelpers.query_wrapper(query))
 
-      assert [
-               %{"id" => "1", "name" => "Me ligue dentro de 5 minutos"},
-               %{"id" => "2", "name" => "Me ligue em um horário específico"},
-               %{"id" => "3", "name" => "Agendamento por e-mail"},
-               %{"id" => "4", "name" => "Agendamento por Whatsapp"}
-             ] = json_response(conn, 200)["data"]["interestTypes"]
-    end
+    assert [
+             %{"id" => "1", "name" => "Me ligue dentro de 5 minutos"},
+             %{"id" => "2", "name" => "Me ligue em um horário específico"},
+             %{"id" => "3", "name" => "Agendamento por e-mail"},
+             %{"id" => "4", "name" => "Agendamento por Whatsapp"}
+           ] == json_response(conn, 200)["data"]["interestTypes"]
+  end
 
-    test "should query interest types for anonymous", %{unauthenticated_conn: conn} do
-      insert(:interest_type, name: "type2", enabled: false)
+  test "should query interest types for anonymous", %{unauthenticated_conn: conn} do
+    insert(:interest_type, name: "type2", enabled: false)
 
-      query = """
-        {
-          interestTypes {
-            id
-            name
-          }
+    query = """
+      query InterestTypes {
+        interestTypes {
+          id
+          name
         }
-      """
+      }
+    """
 
-      conn = post(conn, "/graphql_api", AbsintheHelpers.query_skeleton(query, "dashboard"))
+    conn = post(conn, "/graphql_api", AbsintheHelpers.query_wrapper(query))
 
-      assert [
-               %{"id" => "1", "name" => "Me ligue dentro de 5 minutos"},
-               %{"id" => "2", "name" => "Me ligue em um horário específico"},
-               %{"id" => "3", "name" => "Agendamento por e-mail"},
-               %{"id" => "4", "name" => "Agendamento por Whatsapp"}
-             ] = json_response(conn, 200)["data"]["interestTypes"]
-    end
+    assert [
+             %{"id" => "1", "name" => "Me ligue dentro de 5 minutos"},
+             %{"id" => "2", "name" => "Me ligue em um horário específico"},
+             %{"id" => "3", "name" => "Agendamento por e-mail"},
+             %{"id" => "4", "name" => "Agendamento por Whatsapp"}
+           ] == json_response(conn, 200)["data"]["interestTypes"]
   end
 end
