f841d17cb356f629abfea1af7e3f67efc69c36d6
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-08-23 00:17:53 -0300

    Remove old query skeletons

--- test/support/absinthe_helpers.ex
+++ test/support/absinthe_helpers.ex
@@ -2,26 +2,10 @@ defmodule ReWeb.AbsintheHelpers do
   @moduledoc """
   Helpers for absinthe endpoint testing
   """
-  def query_skeleton(query, query_name) do
-    %{
-      "operationName" => "#{query_name}",
-      "query" => "query #{query_name} #{query}",
-      "variables" => "{}"
-    }
-  end
-
   def query_wrapper(query, variables \\ %{}) do
     %{
       "query" => query,
       "variables" => variables
-    }
-  end
-
-  def mutation_skeleton(query) do
-    %{
-      "operationName" => "",
-      "query" => "#{query}",
-      "variables" => ""
     }
   end
 
