e0123864b9dc6a45a3dfbfff37d70b4143928970
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-10-18 14:39:08 -0300

    Setup honeybadger

--- mix.exs
+++ mix.exs
@@ -15,7 +15,8 @@ defmodule Re.Umbrella.Mixfile do
     [
       {:credo, "~> 0.9", only: [:dev, :test], runtime: false},
       {:timber, "~> 2.6"},
-      {:excoveralls, "~> 0.8", only: :test}
+      {:excoveralls, "~> 0.8", only: :test},
+      {:honeybadger, "~> 0.10"}
     ]
   end
 end
