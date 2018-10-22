fdc399aae2fff4473d24ce0a2aca40bdea5456a0
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-10-22 13:14:35 -0300

    Fix honeybadger config

--- config/prod.exs
+++ config/prod.exs
@@ -77,5 +77,6 @@ config :account_kit,
   app_secret: System.get_env("ACCOUNT_KIT_APP_SECRET")
 
 config :honeybadger,
+  app: :re,
   environment_name: System.get_env("ENV"),
   hostname: System.get_env("HOST")
