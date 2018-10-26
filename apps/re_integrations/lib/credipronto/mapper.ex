41c55a9510d6b55713dbf7f6648fce9a6125708b
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-10-26 18:09:40 -0300

    Add credipronto API query mapper

---
+++ apps/re_integrations/lib/credipronto/mapper.ex
@@ -0,0 +1,83 @@
+defmodule ReIntegrations.Credipronto.Mapper do
+  @moduledoc """
+  Module for mapping Credipronto's query and response payloads
+  """
+
+  def query_out(params), do: Enum.reduce(params, %{}, &map_attributes/2)
+
+  defp map_attributes({:mutuary, value}, acc), do: Map.put(acc, :mutuario, value)
+
+  defp map_attributes({:birthday, value}, acc), do: Map.put(acc, :data_nascimento, encode_date(value))
+
+  defp map_attributes({:include_coparticipant, value}, acc), do: Map.put(acc, :incluir_co, encode_boolean(value))
+
+  defp map_attributes({:net_income, value}, acc), do: Map.put(acc, :renda_liquida, encode_decimal(value))
+
+  defp map_attributes({:amortization, value}, acc), do: Map.put(acc, :amortizacao, encode_boolean(value))
+
+  defp map_attributes({:annual_interest, value}, acc), do: Map.put(acc, :juros_anual, encode_float(value))
+
+  defp map_attributes({:birthday_coparticipant, value}, acc), do: Map.put(acc, :data_nascimento_co, encode_date(value))
+
+  defp map_attributes({:calculate_tr, value}, acc), do: Map.put(acc, :calcular_tr, encode_boolean(value))
+
+  defp map_attributes({:evaluation_rate, value}, acc), do: Map.put(acc, :tarifa_avaliacao, encode_decimal(value))
+
+  defp map_attributes({:fundable_value, value}, acc), do: Map.put(acc, :valor_financiavel, encode_decimal(value))
+
+  defp map_attributes({:home_equity_annual_interest, value}, acc), do: Map.put(acc, :juros_anual_home_equity, encode_float(value))
+
+  defp map_attributes({:insurer, value}, acc), do: Map.put(acc, :seguradora, value)
+
+  defp map_attributes({:itbi_value, value}, acc), do: Map.put(acc, :valor_itbi, encode_decimal(value))
+
+  defp map_attributes({:listing_price, value}, acc), do: Map.put(acc, :valor_imovel, encode_decimal(value))
+
+  defp map_attributes({:listing_type, value}, acc), do: Map.put(acc, :tipo_imovel, value)
+
+  defp map_attributes({:net_income_coparticipant, value}, acc), do: Map.put(acc, :renda_liquida_co, encode_decimal(value))
+
+  defp map_attributes({:product_type, value}, acc), do: Map.put(acc, :tipo_produto, value)
+
+  defp map_attributes({:rating, value}, acc), do: Map.put(acc, :rating, to_string(value))
+
+  defp map_attributes({:sum, value}, acc), do: Map.put(acc, :somar, encode_boolean(value))
+
+  defp map_attributes({:term, value}, acc), do: Map.put(acc, :prazo, to_string(value))
+
+  defp encode_date(nil), do: ""
+
+  defp encode_date(%Date{} = date) do
+    day = date.day
+      |> to_string()
+      |> String.pad_leading(2, "0")
+
+    month = date.month
+      |> to_string()
+      |> String.pad_leading(2, "0")
+
+    year = date.year
+      |> to_string()
+      |> String.pad_leading(2, "0")
+
+    "#{day}/#{month}/#{year}"
+  end
+
+  defp encode_boolean(true), do: "S"
+  defp encode_boolean(_), do: "N"
+
+  defp encode_decimal(nil), do: ""
+
+  defp encode_decimal(%Decimal{} = decimal) do
+    decimal
+    |> Decimal.round(2)
+    |> Decimal.mult(100)
+    |> Decimal.to_integer()
+    |> CurrencyFormatter.format("BRL", keep_decimals: true)
+    |> trim_currency()
+  end
+
+  defp encode_float(float), do: :erlang.float_to_binary(float, [decimals: 10])
+
+  defp trim_currency("R$" <> rest), do: rest
+end
---
+++ apps/re_integrations/test/credipronto/mapper_test.exs
@@ -0,0 +1,55 @@
+defmodule ReIntegrations.Credipronto.MapperTest do
+  use ExUnit.Case
+
+  alias ReIntegrations.Credipronto.Mapper
+
+  describe "query_out/1" do
+    test "should map simulation query to API format" do
+      assert %{
+        mutuario: "PF",
+        data_nascimento: "01/05/1987",
+        incluir_co: "N",
+        renda_liquida: "19.000,00",
+        renda_liquida_co: "",
+        data_nascimento_co: "",
+        tipo_produto: "F",
+        tipo_imovel: "R",
+        valor_imovel: "340.000,00",
+        seguradora: "itau",
+        amortizacao: "S",
+        valor_financiavel: "278.800,00",
+        tarifa_avaliacao: "3.300,00",
+        prazo: "360",
+        calcular_tr: "N",
+        valor_itbi: "2.130,00",
+        juros_anual: "10.5000000000",
+        juros_anual_home_equity: "21.0000000000",
+        rating: "2",
+        somar: "S"
+      } == Mapper.query_out(%{
+        mutuary: "PF",
+        birthday: ~D[1987-05-01],
+        include_coparticipant: false,
+        net_income: Decimal.new(19_000),
+        net_income_coparticipant: nil,
+        birthday_coparticipant: nil,
+        product_type: "F",
+        listing_type: "R",
+        listing_price: Decimal.new(340_000),
+        insurer: "itau",
+        amortization: true,
+        fundable_value: Decimal.new(278_800),
+        evaluation_rate: Decimal.new(3_300),
+        term: 360,
+        calculate_tr: false,
+        itbi_value: Decimal.new(2_130),
+        annual_interest: 10.5,
+        home_equity_annual_interest: 21.0,
+        rating: 2,
+        sum: true
+      })
+    end
+  end
+
+
+end
