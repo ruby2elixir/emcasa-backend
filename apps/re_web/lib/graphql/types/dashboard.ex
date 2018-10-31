b2753d025851971d3e2de315d040b7cfcda3fd00
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-10-31 11:45:44 -0300

    Add admin API for highlight in integrations

--- apps/re_web/lib/graphql/types/dashboard.ex
+++ apps/re_web/lib/graphql/types/dashboard.ex
@@ -22,16 +22,42 @@ defmodule ReWeb.Types.Dashboard do
     field :area_count, :integer, resolve: &DashboardResolvers.area_count/2
   end
 
+  input_object :listing_zap_highlight do
+    field :listing_id, non_null(:id)
+    field :highlight, non_null(:boolean)
+    field :super_highlight, non_null(:boolean)
+  end
+
+  input_object :listing_vivareal_highlight do
+    field :listing_id, non_null(:id)
+    field :highlight, non_null(:boolean)
+  end
+
   object :dashboard_queries do
     @desc "Get dashboard stats"
     field :dashboard, :dashboard, resolve: &DashboardResolvers.index/2
   end
 
   object :dashboard_mutations do
+    @desc "Upload file with price suggestion factors"
     field :upload_factors_csv, :string do
       arg :factors, non_null(:upload)
 
       resolve &DashboardResolvers.upload_factors_csv/2
+    end
+
+    @desc "Bulk highligh listing on zap"
+    field :listing_highlight_zap, :listing do
+      arg :input, list_of(:listing_zap_highlight)
+
+      resolve &DashboardResolvers.listing_highlight_zap/2
+    end
+
+    @desc "Bulk highligh listing on vivareal"
+    field :listing_highlight_vivareal, :listing do
+      arg :input, list_of(:listing_vivareal_highlight)
+
+      resolve &DashboardResolvers.listing_highlight_vivareal/2
     end
   end
 end
