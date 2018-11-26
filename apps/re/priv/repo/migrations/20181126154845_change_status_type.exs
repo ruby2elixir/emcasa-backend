e7c68a3c3d0f0064e2e9e94fc23b0eaa1ba869fc
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-11-26 15:35:36 -0200

    Do not remove is_active field yet

--- apps/re/priv/repo/migrations/20181126154845_change_status_type.exs
+++ apps/re/priv/repo/migrations/20181126154845_change_status_type.exs
@@ -13,29 +13,9 @@ defmodule Re.Repo.Migrations.ChangeStatusType do
     Re.Listing
     |> where([l], l.is_active == true)
     |> Re.Repo.update_all(set: [status: "active"])
-
-    flush()
-
-    alter table(:listings) do
-      remove :is_active
-    end
   end
 
   def down do
-    alter table(:listings) do
-      add :is_active, :boolean, default: false
-    end
-
-    flush()
-
-    import Ecto.Query
-
-    Re.Listing
-    |> where([l], l.status == "active")
-    |> Re.Repo.update_all(set: [is_active: true])
-
-    flush()
-
     alter table(:listings) do
       remove :status
     end
