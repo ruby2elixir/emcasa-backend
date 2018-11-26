ddcacad0fe12d51b73f8f0549bb317e97eec141e
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-11-26 14:39:14 -0200

    Add formatter to re app

---
+++ apps/re/.formatter.exs
@@ -0,0 +1,14 @@
+[
+  inputs: ["mix.exs", "lib/**/*.{ex,exs}"],
+  locals_without_parens: [
+    field: 2,
+    field: 3,
+    has_many: 2,
+    belongs_to: 2,
+    embeds_one: 2,
+    many_to_many: 3,
+    config: 2,
+    enum: 2,
+    has_one: 2
+  ]
+]
