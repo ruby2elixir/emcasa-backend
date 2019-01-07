94ff95c5afb0e7f274e665b87e280db21aacc64c
pmargreff <pmargreff@gmail.com>
2019-01-07 16:17:24 -0200

    Update ecto and related dependencies

--- apps/re/lib/repo.ex
+++ apps/re/lib/repo.ex
@@ -1,4 +1,17 @@
 defmodule Re.Repo do
   use Ecto.Repo, otp_app: :re
   use Scrivener, page_size: 20
+
+  def init(_, opts) do
+    :ok =
+      Telemetry.attach(
+        "timber-ecto-query-handler",
+        [:re, :repo, :query],
+        Timber.Ecto,
+        :handle_event,
+        []
+      )
+
+    {:ok, opts}
+  end
 end
