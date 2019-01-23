cfddc80e30eda407a6d787af183c45474da80cdf
Pablo Margreff <pmargreff@gmail.com>
2019-01-23 19:25:32 -0200

    Format migration

--- apps/re/priv/repo/migrations/20190122191803_remove_zap_highlights_and_zap_super_highlights.exs
+++ apps/re/priv/repo/migrations/20190122191803_remove_zap_highlights_and_zap_super_highlights.exs
@@ -5,7 +5,7 @@ defmodule Re.Repo.Migrations.RemoveZapHighlightsAndZapSuperHighlights do
     drop unique_index(:zap_highlights, [:listing_id])
     drop table(:zap_highlights)
 
-     drop unique_index(:zap_super_highlights, [:listing_id])
+    drop unique_index(:zap_super_highlights, [:listing_id])
     drop table(:zap_super_highlights)
   end
 end
