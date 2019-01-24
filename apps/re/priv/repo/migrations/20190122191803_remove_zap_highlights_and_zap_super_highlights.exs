05f767326dfb23371ca4e22e55ba266af1d615bc
pmargreff <pmargreff@gmail.com>
2019-01-24 10:21:11 -0200

    Fix migration fields for zap removal
        * Original fields have been dropped in other migration, and
        replaced on listings table.

--- apps/re/priv/repo/migrations/20190122191803_remove_zap_highlights_and_zap_super_highlights.exs
+++ apps/re/priv/repo/migrations/20190122191803_remove_zap_highlights_and_zap_super_highlights.exs
@@ -2,10 +2,9 @@ defmodule Re.Repo.Migrations.RemoveZapHighlightsAndZapSuperHighlights do
   use Ecto.Migration
 
   def change do
-    drop unique_index(:zap_highlights, [:listing_id])
-    drop table(:zap_highlights)
-
-    drop unique_index(:zap_super_highlights, [:listing_id])
-    drop table(:zap_super_highlights)
+    alter table(:listings) do
+      remove :zap_highlight
+      remove :zap_super_highlight
+    end
   end
 end
