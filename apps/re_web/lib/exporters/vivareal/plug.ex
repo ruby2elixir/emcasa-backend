f1b972c036dba242c7db858d66a6c46a34b62cec
pmargreff <pmargreff@gmail.com>
2019-01-28 17:42:52 -0200

    Allow highlights to receive city slugs

--- apps/re_web/lib/exporters/vivareal/plug.ex
+++ apps/re_web/lib/exporters/vivareal/plug.ex
@@ -4,13 +4,36 @@ defmodule ReWeb.Exporters.Vivareal.Plug do
   """
   import Plug.Conn
 
-  alias Re.Exporters.Vivareal
+  alias Re.{
+    Exporters.Vivareal,
+    Listings.Exporter,
+  }
 
   def init(args), do: args
 
+  def call(
+        %Plug.Conn{path_info: [state_slug, city_slug], query_params: query_params} = conn,
+        _args
+      ) do
+    filters = %{cities_slug: [city_slug], states_slug: [state_slug]}
+
+    xml_listings =
+      Exporter.exportable(filters, query_params)
+      |> Vivareal.export_listings_xml()
+
+      conn
+    |> put_resp_content_type("application/xml")
+    |> send_resp(200, xml_listings)
+  end
+
   def call(conn, _args) do
+    error_response =
+      {"error", "Expect state and city on path"}
+      |> XmlBuilder.document()
+      |> XmlBuilder.generate(format: :none)
+
     conn
     |> put_resp_content_type("application/xml")
-    |> send_resp(200, Vivareal.export_listings_xml())
+    |> send_resp(404, error_response)
   end
 end
--- apps/re_web/test/exporters/vivareal/plug_test.exs
+++ apps/re_web/test/exporters/vivareal/plug_test.exs
@@ -6,10 +6,32 @@ defmodule ReWeb.Exporters.Vivareal.PlugTest do
   end
 
   @empty_listings ~s|<?xml version="1.0" encoding="UTF-8"?><ListingDataFeed xmlns="http://www.vivareal.com/schemas/1.0/VRSync" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.vivareal.com/schemas/1.0/VRSync  http://xml.vivareal.com/vrsync.xsd"><Header><Provider>EmCasa</Provider><Email>rodrigo.nonose@emcasa.com</Email><ContactName>Rodrigo Nonose</ContactName></Header><Listings/></ListingDataFeed>|
+  @error_message ~s|<?xml version="1.0" encoding="UTF-8"?><error>Expect state and city on path</error>|
 
-  test "should export vivareal XML", %{conn: conn} do
-    conn = get(conn, "/exporters/vivareal")
+  describe "with active city and state slugs on path" do
+    @tag dev: true
+    test "should render listings XML  ", %{conn: conn} do
+      conn = get(conn, "/exporters/vivareal/rj/rio-de-janeiro")
 
-    assert response(conn, 200) == @empty_listings
+      assert response(conn, 200) == @empty_listings
+    end
+  end
+
+  @tag dev: true
+  describe "without city and state slugs on path" do
+    test "should return not found error", %{conn: conn} do
+      conn = get(conn, "/exporters/vivareal")
+
+      assert response(conn, 404) == @error_message
+    end
+  end
+
+  @tag dev: true
+  describe "with invalid slug" do
+    test "should return not found error", %{conn: conn} do
+      conn = get(conn, "/exporters/vivareal/invalid")
+
+      assert response(conn, 404) == @error_message
+    end
   end
 end
