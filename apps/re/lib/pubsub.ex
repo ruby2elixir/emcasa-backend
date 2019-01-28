338bc51aa1618e430c101779932d89f3964c9505
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2019-01-28 13:33:39 -0200

    Refactor pubsub update function

--- apps/re/lib/pubsub.ex
+++ apps/re/lib/pubsub.ex
@@ -17,13 +17,13 @@ defmodule Re.PubSub do
     end
   end
 
-  def publish_update(content, topic) do
+  def publish_update(content, changeset, topic) do
     case content do
       {:ok, content} ->
         Phoenix.PubSub.broadcast(__MODULE__, topic, %{
           topic: topic,
           type: :update,
-          content: content
+          content: %{new: content, changeset: changeset}
         })
 
         {:ok, content}
