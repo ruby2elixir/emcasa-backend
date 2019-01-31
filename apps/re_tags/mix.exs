5ee30d086e742434de8d02f89e76698791136a27
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2019-01-31 13:53:11 -0200

    Add heroku buildpack configuration

--- apps/re_tags/mix.exs
+++ apps/re_tags/mix.exs
@@ -11,6 +11,10 @@ defmodule ReTags.MixProject do
       lockfile: "../../mix.lock",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: Mix.compilers(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
     ]
@@ -22,6 +26,9 @@ defmodule ReTags.MixProject do
       extra_applications: [:logger, :runtime_tools]
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
