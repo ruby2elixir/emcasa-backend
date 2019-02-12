4c12184b4644a9865fc0025fc0774d5288575b13
Joao P Dubas <joao.dubas@gmail.com>
2019-02-12 15:32:58 -0200

    chore(highlights): improve queries test.
    1. Use proper neighborhood names
    2. Use hardcoded expected values
    3. Use proper test description

--- apps/re/test/listings/queries_test.exs
+++ apps/re/test/listings/queries_test.exs
@@ -17,12 +17,12 @@ defmodule Re.Listings.QueriesTest do
       assert [] == result
     end
 
-    test "average equals only listing for neighborhood" do
+    test "average price per area equals to value calculated for the only listing available in neighborhood" do
       data = %{price: 10_000, area: 1_000, address: %{neighborhood_slug: "john-doe"}}
 
       expected = [
         %{
-          average_price_per_area: data.price / data.area,
+          average_price_per_area: 10.0,
           neighborhood_slug: data.address.neighborhood_slug
         }
       ]
@@ -37,20 +37,20 @@ defmodule Re.Listings.QueriesTest do
     end
 
     test "average for multiple neighborhoods" do
-      data_johndoe_1 = %{price: 10_000, area: 1_000, address: %{neighborhood_slug: "john-doe"}}
-      data_johndoe_2 = %{price: 15_000, area: 1_000, address: %{neighborhood_slug: "john-doe"}}
-      data_janedoe_1 = %{price: 20_000, area: 1_000, address: %{neighborhood_slug: "jane-doe"}}
-      data_janedoe_2 = %{price: 20_000, area: 2_000, address: %{neighborhood_slug: "jane-doe"}}
+      data_lapa_1 = %{price: 10_000, area: 1_000, address: %{neighborhood_slug: "lapa"}}
+      data_lapa_2 = %{price: 15_000, area: 1_000, address: %{neighborhood_slug: "lapa"}}
+      data_perdizes_1 = %{price: 20_000, area: 1_000, address: %{neighborhood_slug: "perdizes"}}
+      data_perdizes_2 = %{price: 20_000, area: 2_000, address: %{neighborhood_slug: "perdizes"}}
 
       expected = [
-        %{neighborhood_slug: "jane-doe", average_price_per_area: 15.0},
-        %{neighborhood_slug: "john-doe", average_price_per_area: 12.5}
+        %{neighborhood_slug: "lapa", average_price_per_area: 12.5},
+        %{neighborhood_slug: "perdizes", average_price_per_area: 15.0}
       ]
 
-      insert(:listing, data_johndoe_1)
-      insert(:listing, data_johndoe_2)
-      insert(:listing, data_janedoe_1)
-      insert(:listing, data_janedoe_2)
+      insert(:listing, data_lapa_1)
+      insert(:listing, data_lapa_2)
+      insert(:listing, data_perdizes_1)
+      insert(:listing, data_perdizes_2)
 
       result =
         Queries.average_price_per_area_by_neighborhood()
