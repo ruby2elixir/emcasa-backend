bacd1ce5cf81420eed533928333e1749aebb8036
pmargreff <pmargreff@gmail.com>
2019-02-25 18:11:28 -0300

    wip test structures

---
+++ apps/re_web/lib/graphql/types/development.ex
@@ -0,0 +1,9 @@
+defmodule ReWeb.Types.Development do
+  @module
+  @moduledoc """
+  GraphQL types for developments
+  """
+  use Absinthe.Schema.Notation
+
+  alias ReWeb.Resolvers
+end
---
+++ apps/re_web/test/graphql/developments/query_test.exs
@@ -0,0 +1,27 @@
+
+defmodule ReWeb.GraphQL.Developments.QueryTest do
+  use ReWeb.ConnCase
+
+  import Re.Factory
+
+  alias ReWeb.AbsintheHelpers
+
+  setup %{conn: conn} do
+    conn = put_req_header(conn, "accept", "application/json")
+    admin_user = insert(:user, email: "admin@email.com", role: "admin")
+    user_user = insert(:user, email: "user@email.com", role: "user")
+
+    {:ok,
+     unauthenticated_conn: conn,
+     admin_user: admin_user,
+     user_user: user_user,
+     admin_conn: login_as(conn, admin_user),
+     user_conn: login_as(conn, user_user)}
+  end
+
+  describe "developments" do
+    test "admin should query development index", %{admin_conn: conn} do
+      assert 1 == 1
+    end
+  end
+end
