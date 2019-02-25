f800b4655ff746340aa36c817cc4b5d0b67a8abb
pmargreff <pmargreff@gmail.com>
2019-02-25 15:05:48 -0300

    Cast development association on images

--- apps/re/lib/images/image.ex
+++ apps/re/lib/images/image.ex
@@ -28,7 +28,7 @@ defmodule Re.Image do
     struct
     |> cast(params, @create_required ++ @create_optional)
     |> cast_assoc(:listing)
-    |> cast_assoc(:listing)
+    |> cast_assoc(:development)
     |> validate_required(@create_required)
   end
 
