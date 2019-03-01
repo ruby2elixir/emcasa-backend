0d03c52915576fdbf407939d1ba9101b27f8c1bc
pmargreff <pmargreff@gmail.com>
2019-03-01 09:44:25 -0300

    Rename access permission function

--- apps/re_web/lib/graphql/resolvers/addresses.ex
+++ apps/re_web/lib/graphql/resolvers/addresses.ex
@@ -9,13 +9,13 @@ defmodule ReWeb.Resolvers.Addresses do
   }
 
   def per_listing(listing, params, %{context: %{current_user: current_user}}) do
-    admin? = admin_rights?(listing, current_user)
+    admin? = admin_permissions?(listing, current_user)
 
     {:address, Map.put(params, :has_admin_rights, admin?)}
   end
 
   def per_development(_development, params, %{context: %{current_user: current_user}}) do
-    admin? = admin_rights?(nil, current_user)
+    admin? = admin_permissions?(nil, current_user)
 
     {:address, Map.put(params, :has_admin_rights, admin?)}
   end
@@ -44,7 +44,7 @@ defmodule ReWeb.Resolvers.Addresses do
 
   def is_covered(address, _, _), do: {:ok, Addresses.is_covered(address)}
 
-  defp admin_rights?(%{user_id: user_id}, %{id: user_id}), do: true
-  defp admin_rights?(_, %{role: "admin"}), do: true
-  defp admin_rights?(_, _), do: false
+  defp admin_permissions?(%{user_id: user_id}, %{id: user_id}), do: true
+  defp admin_permissions?(_, %{role: "admin"}), do: true
+  defp admin_permissions?(_, _), do: false
 end
