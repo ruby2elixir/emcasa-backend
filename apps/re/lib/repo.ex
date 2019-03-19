e92146987640b03cebd04839dd7e99d9cc84d3ad
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2019-03-19 16:02:24 -0300

    Fix timber configuration for ecto v3

--- apps/re/lib/repo.ex
+++ apps/re/lib/repo.ex
@@ -1,17 +1,7 @@
 defmodule Re.Repo do
-  use Ecto.Repo, otp_app: :re
+  use Ecto.Repo,
+    otp_app: :re,
+    adapter: Ecto.Adapters.Postgres
+
   use Scrivener, page_size: 20
-
-  def init(_, opts) do
-    :ok =
-      Telemetry.attach(
-        "timber-ecto-query-handler",
-        [:re, :repo, :query],
-        Timber.Ecto,
-        :handle_event,
-        []
-      )
-
-    {:ok, opts}
-  end
 end
