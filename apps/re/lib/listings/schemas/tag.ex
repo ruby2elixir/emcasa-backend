ffc8f64953af4ee9eb6c61d68efb492ad2ae85b7
Joao P Dubas <joao.dubas@gmail.com>
2019-03-19 21:17:56 +0000

    feat(tags) add schema for tag value.

---
+++ apps/re/lib/listings/schemas/tag.ex
@@ -0,0 +1,48 @@
+defmodule Re.Listings.Tag do
+  @moduledoc """
+  Model for tag value
+  """
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  @primary_key {:uuid, :binary_id, autogenerate: false}
+
+  schema "tags" do
+    field :name, :string
+    field :name_slug, :string
+    field :category, :string
+
+    timestamps()
+  end
+
+  @required [:name]
+
+  @sluggfied_attr [:name]
+
+  @categories ~w(infrastructure location realty)
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, @required)
+    |> validate_required(@required)
+    |> unique_constraint(:name_slug)
+    |> validate_inclusion(:category, @categories,
+      message: "should be one of: [#{Enum.join(@categories, ", ")}]"
+    )
+    |> generate_slugs()
+    |> generate_uuid()
+  end
+
+  def generate_slugs(%{valid?: false} = changeset), do: changeset
+
+  def generate_slugs(changeset) do
+    Enum.reduce(@sluggified_attr, changeset, &Slugs.generate_slug(&1, &2))
+  end
+
+  def generate_uuid(%{data: %{uuid: nil}} = changeset) do
+    change(changeset, %{uuid: UUID.uuid4()})
+  end
+
+  def generate_uuid(changeset), do: changeset
+end
---
+++ apps/re/priv/repo/migrations/20190319183837_create_tags.exs
@@ -0,0 +1,16 @@
+defmodule Re.Repo.Migrations.CreateTags do
+  use Ecto.Migration
+
+  def change do
+    create table(:tags, primary_key: false) do
+      add(:uuid, :uuid, primary_key: true)
+      add(:name, :string, null: false)
+      add(:name_slug, :string, null: false)
+      add(:category, :string, null: false, default: "infrastructure")
+
+      timestamps()
+    end
+
+    create unique_index(:tags, [:name_slug])
+  end
+end
