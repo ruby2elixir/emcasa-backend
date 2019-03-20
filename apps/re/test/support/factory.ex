a48d9ac95df82d3fe54714de122082f170611262
Joao P Dubas <joao.dubas@gmail.com>
2019-03-20 14:23:52 +0000

    chore(tests) refac to use slugs module
    `s/Re.Addresses.Slugs/Re.Slugs`

--- apps/re/test/support/factory.ex
+++ apps/re/test/support/factory.ex
@@ -51,16 +51,16 @@ defmodule Re.Factory do
 
   def address_factory do
     street_name = Address.street_name()
-    street_slug = Re.Addresses.Slugs.sluggify(street_name)
+    street_slug = Re.Slugs.sluggify(street_name)
 
     neighborhood_name = Pokemon.location()
-    neighborhood_slug = Re.Addresses.Slugs.sluggify(neighborhood_name)
+    neighborhood_slug = Re.Slugs.sluggify(neighborhood_name)
 
     city_name = Address.city()
-    city_slug = Re.Addresses.Slugs.sluggify(city_name)
+    city_slug = Re.Slugs.sluggify(city_name)
 
     state_name = Address.state_abbr()
-    state_slug = Re.Addresses.Slugs.sluggify(state_name)
+    state_slug = Re.Slugs.sluggify(state_name)
 
     %Re.Address{
       street_number: Address.building_number(),
