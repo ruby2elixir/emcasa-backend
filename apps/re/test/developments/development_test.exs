441ba094fcc058266cd59ebb88511f3c7c099b8b
pmargreff <pmargreff@gmail.com>
2019-03-26 11:56:51 -0300

    Add missing context to development's changeset test

--- apps/re/test/developments/development_test.exs
+++ apps/re/test/developments/development_test.exs
@@ -21,32 +21,35 @@ defmodule Re.DevelopmentTest do
     description: "description"
   }
 
-  test "changeset with valid attributes" do
-    %{id: address_id} = insert(:address)
+  describe "changeset/2" do
+    test "with valid attributes" do
+      %{id: address_id} = insert(:address)
 
-    attrs =
-      @valid_attrs
-      |> Map.put(:address_id, address_id)
+      attrs =
+        @valid_attrs
+        |> Map.put(:address_id, address_id)
 
-    changeset = Development.changeset(%Development{}, attrs)
-    assert changeset.valid?
-  end
+      changeset = Development.changeset(%Development{}, attrs)
+      assert changeset.valid?
+    end
 
-  test "changeset with invalid attributes" do
-    changeset = Development.changeset(%Development{}, @invalid_attrs)
-    refute changeset.valid?
+    test "with invalid attributes" do
+      changeset = Development.changeset(%Development{}, @invalid_attrs)
+      refute changeset.valid?
 
-    assert Keyword.get(changeset.errors, :phase) ==
-             {"should be one of: [pre-launch planning building delivered]",
-              [validation: :inclusion]}
+      assert Keyword.get(changeset.errors, :phase) ==
+               {"should be one of: [pre-launch planning building delivered]",
+                [validation: :inclusion]}
 
-    assert Keyword.get(changeset.errors, :name) == {"can't be blank", [validation: :required]}
+      assert Keyword.get(changeset.errors, :name) == {"can't be blank", [validation: :required]}
 
-    assert Keyword.get(changeset.errors, :title) == {"can't be blank", [validation: :required]}
+      assert Keyword.get(changeset.errors, :title) == {"can't be blank", [validation: :required]}
 
-    assert Keyword.get(changeset.errors, :builder) == {"can't be blank", [validation: :required]}
+      assert Keyword.get(changeset.errors, :builder) ==
+               {"can't be blank", [validation: :required]}
 
-    assert Keyword.get(changeset.errors, :address_id) ==
-             {"can't be blank", [validation: :required]}
+      assert Keyword.get(changeset.errors, :address_id) ==
+               {"can't be blank", [validation: :required]}
+    end
   end
 end
