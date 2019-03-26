31b96bb4f51f0cf1296c0ebeaf8f04b177f2efb4
Joao P Dubas <joao.dubas@gmail.com>
2019-03-26 15:05:27 +0000

    feat(tags) filter listing by tag slug and uuid.

---
+++ apps/re/test/filtering/filtering/tags_uuid_test.exs
@@ -0,0 +1,74 @@
+defmodule Re.Filtering.TagsUUIDTest do
+  use Re.ModelCase
+
+  alias Re.{
+    Filtering,
+    Listing,
+    Listings,
+    Repo
+  }
+
+  import Re.Factory
+
+  describe "apply/2" do
+    test "filter by tag uuid" do
+      tag_1 = insert(:tag, name: "Tag 1", name_slug: "tag-1")
+      tag_2 = insert(:tag, name: "Tag 2", name_slug: "tag-2")
+
+      {:ok, listing_1} =
+        insert(:listing)
+        |> Listings.upsert_tags([tag_1.uuid, tag_2.uuid], %{})
+
+      {:ok, _} =
+        insert(:listing)
+        |> Listings.upsert_tags([tag_2.uuid], %{})
+
+      result =
+        Filtering.apply(Listing, %{tags_uuid: [tag_1.uuid]})
+        |> Repo.all()
+
+      assert 1 == Enum.count(result)
+      assert listing_1.id == Enum.at(result, 0).id
+    end
+
+    test "filter by multiple tags uuids" do
+      tag_1 = insert(:tag, name: "Tag 1", name_slug: "tag-1")
+      tag_2 = insert(:tag, name: "Tag 2", name_slug: "tag-2")
+      tag_3 = insert(:tag, name: "Tag 3", name_slug: "tag-3")
+
+      {:ok, listing_1} =
+        insert(:listing)
+        |> Listings.upsert_tags([tag_1.uuid, tag_2.uuid, tag_3.uuid], %{})
+
+      {:ok, listing_2} =
+        insert(:listing)
+        |> Listings.upsert_tags([tag_2.uuid, tag_3.uuid], %{})
+
+      result =
+        Filtering.apply(Listing, %{tags_uuid: [tag_2.uuid, tag_3.uuid]})
+        |> Repo.all()
+
+      assert 2 == Enum.count(result)
+      assert Enum.member?(Enum.map(result, & &1.id), listing_1.id)
+      assert Enum.member?(Enum.map(result, & &1.id), listing_2.id)
+    end
+
+    test "filter by non-existent tag uuid" do
+      tag_1 = insert(:tag, name: "Tag 1", name_slug: "tag-1")
+
+      {:ok, _} =
+        insert(:listing)
+        |> Listings.upsert_tags([tag_1.uuid], %{})
+
+      {:ok, _} =
+        insert(:listing)
+        |> Listings.upsert_tags([tag_1.uuid], %{})
+
+      result =
+        Filtering.apply(Listing, %{tags_uuid: [UUID.uuid4()]})
+        |> Repo.all()
+
+      assert 0 == Enum.count(result)
+    end
+  end
+end
