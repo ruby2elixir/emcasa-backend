8270423a5711b5f35ac8edea4053ffd3979363cd
Joao P Dubas <joao.dubas@gmail.com>
2019-04-01 13:08:18 +0000

    chore(tags) populate tags defined in document.

--- apps/re/priv/repo/migrations/20190320184138_populate_tags.exs
+++ apps/re/priv/repo/migrations/20190320184138_populate_tags.exs
@@ -2,22 +2,44 @@ defmodule Re.Repo.Migrations.PopulateTags do
   use Ecto.Migration
 
   def up do
-    [
-      "Bicicletário",
-      "Salão de Jogos",
-      "Parque",
-      "Espaço verde",
-      "Espaço gourmet",
-      "Quadra",
-      "Playground",
-      "Sauna",
-      "Academia",
-      "Salão de festas",
-      "Churrasqueira",
-      "Piscina"
+    tags = [
+      %{category: "infrastructure", name: "Academia", visibility: "all"},
+      %{category: "infrastructure", name: "Churrasqueira", visibility: "all"},
+      %{category: "infrastructure", name: "Espaço Gourmet", visibility: "all"},
+      %{category: "infrastructure", name: "Espaço Verde", visibility: "all"},
+      %{category: "infrastructure", name: "Parque", visibility: "all"},
+      %{category: "infrastructure", name: "Piscina", visibility: "all"},
+      %{category: "infrastructure", name: "Playground", visibility: "all"},
+      %{category: "infrastructure", name: "Quadra", visibility: "all"},
+      %{category: "infrastructure", name: "Salão De Festas", visibility: "all"},
+      %{category: "infrastructure", name: "Salão De Jogos", visibility: "all"},
+      %{category: "infrastructure", name: "Sauna", visibility: "all"},
+      %{category: "realty", name: "Armários Embutidos", visibility: "all"},
+      %{category: "realty", name: "Banheiro Empregados", visibility: "all"},
+      %{category: "realty", name: "Bom Para Pets", visibility: "all"},
+      %{category: "realty", name: "Dependência Empregados", visibility: "all"},
+      %{category: "realty", name: "Espaço Para Churrasco", visibility: "all"},
+      %{category: "realty", name: "Fogão Embutido", visibility: "all"},
+      %{category: "realty", name: "Lavabo", visibility: "all"},
+      %{category: "realty", name: "Reformado", visibility: "all"},
+      %{category: "realty", name: "Sacada", visibility: "all"},
+      %{category: "realty", name: "Terraço", visibility: "all"},
+      %{category: "realty", name: "Vaga Na Escritura", visibility: "all"},
+      %{category: "realty", name: "Varanda", visibility: "all"},
+      %{category: "realty", name: "Varanda Gourmet", visibility: "all"},
+      %{category: "view", name: "Comunidade", visibility: "admin"},
+      %{category: "view", name: "Cristo", visibility: "all"},
+      %{category: "view", name: "Lagoa", visibility: "all"},
+      %{category: "view", name: "Mar", visibility: "all"},
+      %{category: "view", name: "Montanhas", visibility: "all"},
+      %{category: "view", name: "Parcial Comunidade", visibility: "admin"},
+      %{category: "view", name: "Parcial Mar", visibility: "all"},
+      %{category: "view", name: "Pedras", visibility: "all"},
+      %{category: "view", name: "Verde", visibility: "all"},
+      %{category: "view", name: "Vizinho", visibility: "admin"}
     ]
-    |> Enum.map(fn name -> %{category: "infrastructure", name: name} end)
-    |> Enum.map(&Re.Tags.insert/1)
+
+    Enum.map(tags, &Re.Tags.insert/1)
   end
 
   def down do
