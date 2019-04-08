9296cce6214499ef07c9e46518a4a59f173808a1
Joao P Dubas <joao.dubas@gmail.com>
2019-04-08 15:06:43 +0000

    chore(tags) update error atom to anonymous user in policy.

--- apps/re/lib/tags/policy.ex
+++ apps/re/lib/tags/policy.ex
@@ -6,6 +6,6 @@ defmodule Re.Tags.Policy do
   alias Re.User
 
   def authorize(_, %User{role: "admin"}, _), do: :ok
-  def authorize(_, nil, _), do: {:error, :unauthorize}
+  def authorize(_, nil, _), do: {:error, :unauthorized}
   def authorize(_, _, _), do: {:error, :forbidden}
 end
