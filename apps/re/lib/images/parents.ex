206b9a22cceba31bb342f7c382ca84cded36282b
pmargreff <pmargreff@gmail.com>
2019-04-11 17:09:40 -0300

    Remove abstraction from conditions and getters

--- apps/re/lib/images/parents.ex
+++ apps/re/lib/images/parents.ex
@@ -10,36 +10,17 @@ defmodule Re.Images.Parents do
 
   def get_parent_from_image_list([%{listing_id: listing_id} | _] = images)
       when not is_nil(listing_id) do
-    case unique_listing_parent?(images) do
-      true -> get_listing_parent(images)
-      false -> {:error, :distinct_parents}
+    case Enum.uniq_by(images, fn image -> Map.get(image, :listing_id) end) do
+      [%{listing_id: _}] -> Listings.get(listing_id)
+      _ -> {:error, :distinct_parents}
     end
   end
 
   def get_parent_from_image_list([%{development_uuid: development_uuid} | _] = images)
       when not is_nil(development_uuid) do
-    case unique_development_parent?(images) do
-      true -> get_development_parent(images)
-      false -> {:error, :distinct_parents}
-    end
-  end
-
-  defp unique_listing_parent?(images) do
-    case Enum.uniq_by(images, fn image -> Map.get(image, :listing_id) end) do
-      [%{listing_id: _}] -> true
-      _ -> false
-    end
-  end
-
-  defp unique_development_parent?(images) do
     case Enum.uniq_by(images, fn image -> Map.get(image, :development_uuid) end) do
-      [%{development_uuid: _}] -> true
-      _ -> false
+      [%{development_uuid: _}] -> Developments.get(development_uuid)
+      _ -> {:error, :distinct_parents}
     end
   end
-
-  defp get_listing_parent([%{listing_id: listing_id} | _]), do: Listings.get(listing_id)
-
-  defp get_development_parent([%{development_uuid: development_uuid} | _]),
-    do: Developments.get(development_uuid)
 end
