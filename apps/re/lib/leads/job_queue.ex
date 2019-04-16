7f8736cd227aa699d3a791508ae70ba52faab6dd
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2019-04-16 16:31:09 -0300

    feat(buyer_leads): process grupozap buyer lead use ecto_job to process asynchronously grupozap's buyer lead into a digested lead, where if it's possible, associate user and listing

--- apps/re/lib/leads/job_queue.ex
+++ apps/re/lib/leads/job_queue.ex
@@ -1,7 +1,59 @@
 defmodule Re.Leads.Buyer.JobQueue do
   use EctoJob.JobQueue, table_name: "buyer_leads_jobs"
 
+  require Logger
+
+  alias Re.{
+    Leads.GrupozapBuyer,
+    Leads.Buyer,
+    Listing,
+    Repo,
+    User
+  }
+
   alias Ecto.Multi
 
-  def perform(%Multi{} = multi, %{} = job), do: multi
+  def perform(%Multi{} = multi, %{"type" => "grupozap_buyer_lead", "uuid" => uuid} = job) do
+    GrupozapBuyer
+    |> Repo.get(uuid)
+    |> buyer_lead_changeset()
+    |> persist(multi)
+  end
+
+  def perform(multi, job) do
+    Logger.warn("Job format not handled. Job: #{Kernel.inspect(job)}")
+
+    raise "Job not handled"
+  end
+
+  defp buyer_lead_changeset(nil), do: raise("Leads.GrupozapBuyer not found")
+
+  defp buyer_lead_changeset(gzb) do
+    phone_number = "+55" <> gzb.ddd <> gzb.phone
+
+    Buyer.changeset(%Buyer{}, %{
+      name: gzb.name,
+      email: gzb.email,
+      phone_number: phone_number,
+      origin: gzb.lead_origin,
+      user_uuid: extract_user_uuid(phone_number),
+      listing_uuid: extract_listing_uuid(gzb.client_listing_id)
+    })
+  end
+
+  defp persist(changeset, multi), do: Multi.insert(multi, :insert_buyer_lead, changeset)
+
+  defp extract_user_uuid(phone_number) do
+    case Repo.get_by(User, phone: phone_number) do
+      nil -> nil
+      user -> user.uuid
+    end
+  end
+
+  defp extract_listing_uuid(id) do
+    case Repo.get(Listing, id) do
+      nil -> nil
+      listing -> listing.uuid
+    end
+  end
 end
