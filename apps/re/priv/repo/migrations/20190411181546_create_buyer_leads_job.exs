24f4a3b665c43c33879781c65c0169e5cb847b3d
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2019-04-16 17:00:46 -0300

    style(buyer_leads): alias nested module calls

--- apps/re/priv/repo/migrations/20190411181546_create_buyer_leads_job.exs
+++ apps/re/priv/repo/migrations/20190411181546_create_buyer_leads_job.exs
@@ -1,13 +1,18 @@
 defmodule Re.Repo.Migrations.CreateBuyerLeadsJob do
   use Ecto.Migration
 
+  alias EctoJob.Migrations.{
+    CreateJobTable,
+    Install
+  }
+
   def up do
-    EctoJob.Migrations.Install.up()
-    EctoJob.Migrations.CreateJobTable.up("buyer_leads_jobs")
+    Install.up()
+    CreateJobTable.up("buyer_leads_jobs")
   end
 
   def down do
-    EctoJob.Migrations.CreateJobTable.down("buyer_leads_jobs")
-    EctoJob.Migrations.Install.down()
+    CreateJobTable.down("buyer_leads_jobs")
+    Install.down()
   end
 end
