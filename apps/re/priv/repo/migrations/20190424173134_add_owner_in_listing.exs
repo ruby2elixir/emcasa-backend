904f5b3ad4d8f1b7c8d12c53e4ec8027bcf123b0
Joao P Dubas <joao.dubas@gmail.com>
2019-04-24 18:41:56 +0000

    feat(owner) reference owner in listing.

---
+++ apps/re/priv/repo/migrations/20190424173134_add_owner_in_listing.exs
@@ -0,0 +1,9 @@
+defmodule Re.Repo.Migrations.AddOwnerInListing do
+  use Ecto.Migration
+
+  def change do
+    alter table(:listings) do
+      add :owner_uuid, references(:owner_contacts, column: :uuid, type: :uuid)
+    end
+  end
+end
