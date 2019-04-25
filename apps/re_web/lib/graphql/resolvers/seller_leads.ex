3bcccc5e018a9df24969b152f89d814c00256000
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2019-04-25 14:55:13 -0300

    fix(seller_leads): fix unused import warning

--- apps/re_web/lib/graphql/resolvers/seller_leads.ex
+++ apps/re_web/lib/graphql/resolvers/seller_leads.ex
@@ -2,8 +2,6 @@ defmodule ReWeb.Resolvers.SellerLeads do
   @moduledoc """
   Resolver module for seller leads
   """
-  import Absinthe.Resolution.Helpers, only: [on_load: 2]
-
   alias Re.SellerLeads
 
   def create_site(%{input: params}, %{context: %{current_user: current_user}}) do
