5e1231619aa379e338e5c9f9d5e67776ae8b4be9
pmargreff <pmargreff@gmail.com>
2019-05-04 10:53:18 -0300

    refactor(stats-counter): add alias on statiscts module

--- apps/re/lib/statistics/statistics.ex
+++ apps/re/lib/statistics/statistics.ex
@@ -3,8 +3,10 @@ defmodule Re.Statistics do
   Context for statistics
   """
 
+  alias Re.Statistics.DataloaderQueries
+
   def data(_params), do: Dataloader.Ecto.new(Re.Repo, run_batch: &run_batch/5)
 
   def run_batch(queryable, query, col, inputs, repo_opts),
-    do: Re.Statistics.DataloaderQueries.run_batch(queryable, query, col, inputs, repo_opts)
+    do: DataloaderQueries.run_batch(queryable, query, col, inputs, repo_opts)
 end
