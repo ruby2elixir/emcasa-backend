e02099e4b93d92160ef2a67b508b0862bdbe43da
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2019-05-06 10:56:50 -0300

    style(buyer_leads): run formatter on migration

--- apps/re/priv/repo/migrations/20190506132520_generate_interests_uuid.exs
+++ apps/re/priv/repo/migrations/20190506132520_generate_interests_uuid.exs
@@ -7,7 +7,10 @@ defmodule Re.Repo.Migrations.GenerateInterestsUuid do
     Ecto.Query.from(i in "interests", select: i.id)
     |> Re.Repo.all()
     |> Enum.map(
-      &Ecto.Query.from(i in "interests", where: i.id == ^&1, update: [set: [uuid: ^Ecto.UUID.bingenerate()]])
+      &Ecto.Query.from(i in "interests",
+        where: i.id == ^&1,
+        update: [set: [uuid: ^Ecto.UUID.bingenerate()]]
+      )
     )
     |> Enum.map(&Re.Repo.update_all(&1, []))
   end
