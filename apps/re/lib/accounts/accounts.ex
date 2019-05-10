3baa1c5aef7a477bcf90103093a95a8321c2379d
pmargreff <pmargreff@gmail.com>
2019-05-10 16:26:15 -0300

    refactor(user-management): move user get to users context

--- apps/re/lib/accounts/accounts.ex
+++ apps/re/lib/accounts/accounts.ex
@@ -12,13 +12,6 @@ defmodule Re.Accounts do
 
   def query(query, _args), do: query
 
-  def get_by_uuid(uuid) do
-    case Repo.get_by(User, uuid: uuid) do
-      nil -> {:error, :not_found}
-      user -> {:ok, user}
-    end
-  end
-
   def promote_user_to_admin(user) do
     user
     |> User.update_changeset(%{role: "admin"})
