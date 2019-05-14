56ed1888c89f5b06af4814b4fd0890cdf7eeac3c
pmargreff <pmargreff@gmail.com>
2019-05-14 13:58:09 -0300

    feature(user-management): create user search core

---
+++ apps/re/lib/accounts/filtering.ex
@@ -0,0 +1,41 @@
+defmodule Re.Accounts.Filtering do
+  @moduledoc """
+  Module for grouping accounts filter queries
+  """
+  use Ecto.Schema
+
+  import Ecto.{
+    Query,
+    Changeset
+  }
+
+  schema "account_filter" do
+    field :search, :string
+  end
+
+  @filters ~w(search)a
+
+  def changeset(struct, params \\ %{}), do: cast(struct, params, @filters)
+
+  def apply(query, params) do
+    params
+    |> cast()
+    |> build_query(query)
+  end
+
+  def cast(params) do
+    %__MODULE__{}
+    |> changeset(params)
+    |> Map.get(:changes)
+  end
+
+  defp build_query(params, query), do: Enum.reduce(params, query, &attr_filter/2)
+
+  defp attr_filter({:search, search}, query) do
+    from(l in query, where:
+      like(l.phone, ^"%#{search}%") or
+      ilike(l.name, ^"%#{search}%") or
+      ilike(l.email, ^"%#{search}%")
+    )
+  end
+end
---
+++ apps/re/test/accounts/filtering_test.exs
@@ -0,0 +1,48 @@
+defmodule Re.Accounts.FilteringTest do
+  use Re.ModelCase
+
+  alias Re.{
+    Accounts.Filtering,
+    Repo
+  }
+
+  import Re.Factory
+
+  describe "apply/2" do
+    @tag dev: true
+    test "filter by phone number" do
+      user_1 = insert(:user, phone: "(99)999999999")
+      insert(:user, phone: "(88)88888888")
+
+      result =
+        Filtering.apply(Re.User, %{search: "999"})
+        |> Repo.all()
+
+      assert result == [user_1]
+    end
+
+    @tag dev: true
+    test "filter by user name not considering case" do
+      user_1 = insert(:user, name: "Alice")
+      insert(:user, name: "Bob")
+
+      result =
+        Filtering.apply(Re.User, %{search: "ali"})
+        |> Repo.all()
+
+      assert result == [user_1]
+    end
+
+    @tag dev: true
+    test "filter by user email not considering case" do
+      user_1 = insert(:user, email: "alice@gmail.com")
+      insert(:user, email: "bob@bol.com")
+
+      result =
+        Filtering.apply(Re.User, %{search: "GMAIL"})
+        |> Repo.all()
+
+      assert result == [user_1]
+    end
+  end
+end
