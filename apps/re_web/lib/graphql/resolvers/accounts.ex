4ef93c0bb76a8b7364e7217482b788d14cfa4a5c
pmargreff <pmargreff@gmail.com>
2019-05-14 09:11:54 -0300

    fix(user-management): doesn't overide pagination params

--- apps/re_web/lib/graphql/resolvers/accounts.ex
+++ apps/re_web/lib/graphql/resolvers/accounts.ex
@@ -81,8 +81,10 @@ defmodule ReWeb.Resolvers.Accounts do
   def users(params, %{context: %{current_user: current_user}}) do
     pagination = Map.get(params, :pagination, %{})
 
+    params = Map.merge(params, pagination)
+
     case Bodyguard.permit(Users, :list_users, current_user) do
-      :ok -> {:ok, Re.Accounts.paginated(pagination)}
+      :ok -> {:ok, Re.Accounts.paginated(params)}
       error -> error
     end
   end
