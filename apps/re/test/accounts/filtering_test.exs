1462381523318a1a89164b9dfa4067878a746bc2
pmargreff <pmargreff@gmail.com>
2019-05-14 14:51:44 -0300

    refactor(user-management: remove flip tests from filters
    * Use plain structures instead of factories so we remove
    the chance from match by random values.

--- apps/re/test/accounts/filtering_test.exs
+++ apps/re/test/accounts/filtering_test.exs
@@ -3,43 +3,53 @@ defmodule Re.Accounts.FilteringTest do
 
   alias Re.{
     Accounts.Filtering,
-    Repo
+    Repo,
+    User
   }
 
   import Re.Factory
 
   describe "apply/2" do
     test "filter by phone number" do
-      user_1 = insert(:user, phone: "(99)999999999")
-      insert(:user, phone: "(88)88888888")
+      user = insert(:user, phone: "(99)999999999")
+
+      {:ok, _other_user} =
+        %User{phone: "(88)888888888"}
+        |> Repo.insert()
 
       result =
-        Filtering.apply(Re.User, %{search: "999"})
+        Filtering.apply(User, %{search: "999"})
         |> Repo.all()
 
-      assert result == [user_1]
+      assert result == [user]
     end
 
     test "filter by user name not considering case" do
-      user_1 = insert(:user, name: "Alice")
-      insert(:user, name: "Bob")
+      user = insert(:user, name: "Alice")
+
+      {:ok, _other_user} =
+        %User{name: "Bob"}
+        |> Repo.insert()
 
       result =
-        Filtering.apply(Re.User, %{search: "ali"})
+        Filtering.apply(User, %{search: "ali"})
         |> Repo.all()
 
-      assert result == [user_1]
+      assert result == [user]
     end
 
     test "filter by user email not considering case" do
-      user_1 = insert(:user, email: "alice@gmail.com")
-      insert(:user, email: "bob@bol.com")
+      user = insert(:user, email: "alice@gmail.com")
+
+      {:ok, _other_user} =
+        %User{email: "bob@bol.com"}
+        |> Repo.insert()
 
       result =
-        Filtering.apply(Re.User, %{search: "GMAIL"})
+        Filtering.apply(User, %{search: "GMAIL"})
         |> Repo.all()
 
-      assert result == [user_1]
+      assert result == [user]
     end
   end
 end
