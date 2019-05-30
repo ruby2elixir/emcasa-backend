7f16362882e1b30e417c720778bf9866c6c0e50a
pmargreff <pmargreff@gmail.com>
2019-05-30 16:18:28 -0300

    fix(orulo): alter map keys from atoms to strings

--- apps/re_integrations/lib/orulo/mapper.ex
+++ apps/re_integrations/lib/orulo/mapper.ex
@@ -10,20 +10,20 @@ defmodule ReIntegrations.Orulo.Mapper do
     Enum.reduce(payload, %{}, &convert_development_attribute(&1, &2))
   end
 
-  defp convert_development_attribute({:name, name}, acc), do: Map.put(acc, :name, name)
+  defp convert_development_attribute({"name", name}, acc), do: Map.put(acc, :name, name)
 
-  defp convert_development_attribute({:description, description}, acc),
+  defp convert_development_attribute({"description", description}, acc),
     do: Map.put(acc, :description, description)
 
-  defp convert_development_attribute({:developer, %{name: name}}, acc) do
+  defp convert_development_attribute({"developer", %{"name" => name}}, acc) do
     Map.put(acc, :builder, name)
   end
 
-  defp convert_development_attribute({:number_of_floors, floor_count}, acc) do
+  defp convert_development_attribute({"number_of_floors", floor_count}, acc) do
     Map.put(acc, :floor_count, floor_count)
   end
 
-  defp convert_development_attribute({:apts_per_floor, units_per_floor}, acc) do
+  defp convert_development_attribute({"apts_per_floor", units_per_floor}, acc) do
     Map.put(acc, :units_per_floor, units_per_floor)
   end
 
@@ -33,47 +33,47 @@ defmodule ReIntegrations.Orulo.Mapper do
     "Pronto usado" => "delivered"
   }
 
-  defp convert_development_attribute({:status, status}, acc) do
+  defp convert_development_attribute({"status", status}, acc) do
     phase = Map.get(@phase_map, status)
     Map.put(acc, :phase, phase)
   end
 
   defp convert_development_attribute(_, acc), do: acc
 
-  def building_payload_into_address_params(%Building{} = %{payload: %{address: address}}) do
+  def building_payload_into_address_params(%Building{} = %{payload: %{"address" => address}}) do
     Enum.reduce(address, %{}, &convert_address_attribute(&1, &2))
   end
 
-  defp convert_address_attribute({:street, street}, acc) do
+  defp convert_address_attribute({"street", street}, acc) do
     Map.put(acc, :street, street)
   end
 
-  defp convert_address_attribute({:area, neighborhood}, acc) do
+  defp convert_address_attribute({"area", neighborhood}, acc) do
     Map.put(acc, :neighborhood, neighborhood)
   end
 
-  defp convert_address_attribute({:city, city}, acc) do
+  defp convert_address_attribute({"city", city}, acc) do
     Map.put(acc, :city, city)
   end
 
-  defp convert_address_attribute({:state, state}, acc) do
+  defp convert_address_attribute({"state", state}, acc) do
     Map.put(acc, :state, state)
   end
 
-  defp convert_address_attribute({:zip_code, postal_code}, acc) do
+  defp convert_address_attribute({"zip_code", postal_code}, acc) do
     Map.put(acc, :postal_code, postal_code)
   end
 
-  defp convert_address_attribute({:latitude, lat}, acc) do
+  defp convert_address_attribute({"latitude", lat}, acc) do
     Map.put(acc, :lat, lat)
   end
 
-  defp convert_address_attribute({:longitude, lng}, acc) do
+  defp convert_address_attribute({"longitude", lng}, acc) do
     Map.put(acc, :lng, lng)
   end
 
-  defp convert_address_attribute({:number, number}, acc) do
-    Map.put(acc, :street_number, number)
+  defp convert_address_attribute({"number", number}, acc) do
+    Map.put(acc, :street_number, Integer.to_string(number))
   end
 
   defp convert_address_attribute(_, acc), do: acc
--- apps/re_integrations/test/orulo/mapper_test.exs
+++ apps/re_integrations/test/orulo/mapper_test.exs
@@ -11,44 +11,44 @@ defmodule ReIntegrations.Orulo.MapperTest do
   @building %Building{
     external_id: 999,
     payload: %{
-      id: "999",
-      name: "EmCasa 01",
-      description:
+      "id" => "999",
+      "name" => "EmCasa 01",
+      "description" =>
         "Com 3 dormitórios e espaços amplos, o apartamento foi desenhado de uma forma que permite ventilação e iluminação natural e generosas em praticamente todos os seus ambientes – e funciona quase como uma casa solta no ar. Na melhor localização de Perdizes: com ótimas escolas, restaurantes e lojinhas simpáticas no entorno.",
-      floor_area: 0.0,
-      apts_per_floor: 2,
-      number_of_floors: 8,
-      status: "Em construção",
-      webpage: "http://www.emcasa.com/",
-      developer: %{
-        id: "799",
-        name: "EmCasa Incorporadora"
+      "floor_area" => 0.0,
+      "apts_per_floor" => 2,
+      "number_of_floors" => 8,
+      "status" => "Em construção",
+      "webpage" => "http://www.emcasa.com/",
+      "developer" => %{
+        "id" => "799",
+        "name" => "EmCasa Incorporadora"
       },
-      address: %{
-        street_type: "Avenida",
-        street: "Copacabana",
-        number: 926,
-        area: "Copacabana",
-        city: "Rio de Janeiro",
-        latitude: -23.5345,
-        longitude: -46.6871,
-        state: "RJ",
-        zip_code: "05021-001"
+      "address" => %{
+        "street_type" => "Avenida",
+        "street" => "Copacabana",
+        "number" => 926,
+        "area" => "Copacabana",
+        "city" => "Rio de Janeiro",
+        "latitude" => -23.5345,
+        "longitude" => -46.6871,
+        "state" => "RJ",
+        "zip_code" => "05021-001"
       }
     }
   }
 
   describe "building_payload_into_development_params" do
     test "parse building payload into development" do
-      %{payload: %{developer: developer} = payload} = @building
+      %{payload: %{"developer" => developer} = payload} = @building
       params = Mapper.building_payload_into_development_params(@building)
 
       assert params == %{
-               name: Map.get(payload, :name),
-               description: Map.get(payload, :description),
-               builder: "EmCasa Incorporadora",
-               floor_count: 8,
-               units_per_floor: 2,
+               name: Map.get(payload, "name"),
+               description: Map.get(payload, "description"),
+               builder: Map.get(developer, "name"),
+               floor_count: Map.get(payload, "number_of_floors"),
+               units_per_floor: Map.get(payload, "apts_per_floor"),
                phase: "building"
              }
     end
@@ -56,18 +56,18 @@ defmodule ReIntegrations.Orulo.MapperTest do
 
   describe "building_payload_into_address_params" do
     test "parse building payload into address params" do
-      %{payload: %{address: address}} = @building
+      %{payload: %{"address" => address}} = @building
       params = Mapper.building_payload_into_address_params(@building)
 
       assert params == %{
-               street: Map.get(address, :street),
-               neighborhood: Map.get(address, :area),
-               city: Map.get(address, :city),
-               state: Map.get(address, :state),
-               postal_code: Map.get(address, :zip_code),
-               lat: Map.get(address, :latitude),
-               lng: Map.get(address, :longitude),
-               street_number: Map.get(address, :number)
+               street: Map.get(address, "street"),
+               neighborhood: Map.get(address, "area"),
+               city: Map.get(address, "city"),
+               state: Map.get(address, "state"),
+               postal_code: Map.get(address, "zip_code"),
+               lat: Map.get(address, "latitude"),
+               lng: Map.get(address, "longitude"),
+               street_number: Map.get(address, "number") |> Integer.to_string()
              }
     end
   end
