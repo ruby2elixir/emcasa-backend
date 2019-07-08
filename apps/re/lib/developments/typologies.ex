506977f9685794b084b9aade465f50f27aad4983
Gabriela Seabra <gabriela.lima.se@gmail.com>
2019-07-08 19:30:07 -0300

    refactor(developments) refactor typologies run_batch

--- apps/re/lib/developments/typologies.ex
+++ apps/re/lib/developments/typologies.ex
@@ -4,29 +4,16 @@ defmodule Re.Developments.Typologies do
   """
 
   alias Re.{
-    Listing,
     Development,
+    Developments,
     Repo
   }
-
-  import Ecto.Query
 
   def data(params),
     do: Dataloader.Ecto.new(Re.Repo, run_batch: &run_batch/5, default_params: params)
 
   def run_batch(Development, _query, col, development_uuids, _repo_opts) do
-    Listing
-    |> select([l], %{
-      area: l.area,
-      rooms: l.rooms,
-      max_price: max(l.price),
-      min_price: min(l.price),
-      unit_count: count(l.id),
-      development_uuid: l.development_uuid
-    })
-    |> group_by([l], [l.area, l.rooms, l.development_uuid])
-    |> where([l], l.development_uuid in ^development_uuids and l.status == "active")
-    |> Repo.all()
+    Developments.Listings.typologies(development_uuids)
     |> Enum.reduce(%{}, fn typology, map ->
       Map.update(
         map,
