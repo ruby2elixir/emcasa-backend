2a7e24e931214d219bc2f43d0ee87b5a3df914f5
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2019-07-16 11:16:28 -0300

    feat(seller_leads): add uuid to price suggestion request

---
+++ apps/re/priv/repo/migrations/20190716141320_add_price_suggestion_request_uuid.exs
@@ -0,0 +1,32 @@
+defmodule Re.Repo.Migrations.AddPriceSuggestionRequestUuid do
+  use Ecto.Migration
+
+  alias Re.{
+    PriceSuggestions.Request,
+    Repo
+  }
+
+  def up do
+    alter table(:price_suggestion_requests) do
+      add :uuid, :uuid
+      add :seller_lead_uuid, references(:seller_leads, column: :uuid, type: :uuid)
+    end
+
+    flush()
+
+    Request
+    |> Repo.all()
+    |> Enum.map(fn req ->
+      req
+      |> Request.changeset()
+      |> Repo.update()
+    end)
+  end
+
+  def down do
+    alter table(:price_suggestion_requests) do
+      remove :uuid
+      remove :seller_lead_uuid
+    end
+  end
+end
