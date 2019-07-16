6ac562fdf05c404857f3b7d5a2ed10b52b7312be
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2019-07-16 19:03:30 -0300

    fix(seller_leads): clean new job_queue to remove warnings

--- apps/re/lib/seller_leads/job_queue.ex
+++ apps/re/lib/seller_leads/job_queue.ex
@@ -4,27 +4,5 @@ defmodule Re.SellerLeads.JobQueue do
   """
   use EctoJob.JobQueue, table_name: "seller_lead_jobs"
 
-  require Ecto.Query
-  require Logger
-
-  alias Re.{
-    PriceSuggestions.Request,
-    Repo
-  }
-
-  alias Ecto.{
-    Changeset,
-    Multi,
-    Query
-  }
-
-  def perform(%Multi{} = multi, %{"type" => "process_price_suggestion_request", "uuid" => uuid}) do
-    :ok
-    # Request
-    # |> Repo.get(uuid)
-    # |> Grupozap.buyer_lead_changeset()
-    # |> insert_buyer_lead(multi)
-    # |> Repo.transaction()
-    # |> handle_error()
-  end
+  def perform(_, _), do: :ok
 end
