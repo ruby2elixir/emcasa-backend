eee1c5ef2065f8ca390d21da0af5439ef8a7b405
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2019-07-29 14:23:45 -0300

    feat(aliketeller): extract logic from server and put on context and client

--- apps/re/lib/aliketeller/aliketeller.ex
+++ apps/re/lib/aliketeller/aliketeller.ex
@@ -2,6 +2,12 @@ defmodule Re.AlikeTeller do
   @moduledoc """
   Module to interface with aliketeller for related listings
   """
+  require Logger
+
+  alias __MODULE__.{
+    Client,
+    Server
+  }
 
   @spec get(String.t()) :: {:error, :not_found} | {:ok, list(String.t())}
   def get(uuid) do
@@ -11,5 +17,35 @@ defmodule Re.AlikeTeller do
     end
   end
 
-  def load, do: GenServer.cast(__MODULE__.Server, :load_aliketeller)
+  def load, do: GenServer.cast(Server, :load_aliketeller)
+
+  def create_ets_table do
+    case :ets.whereis(:aliketeller) do
+      :undefined ->
+        :ets.new(:aliketeller, [:set, :protected, :named_table, read_concurrency: true])
+
+      _ ->
+        :ok
+    end
+  end
+
+  def load_aliketeller do
+    create_ets_table()
+
+    with {:ok, %{status_code: 200, body: body}} <- Client.get_payload(),
+         {:ok, payload} <- Jason.decode(body) do
+      save_on_ets(payload)
+    else
+      error ->
+        Logger.warn("Error loading aliketeller payload: #{inspect(error)}")
+
+        error
+    end
+  end
+
+  defp save_on_ets(%{"data" => data}), do: Enum.each(data, &do_save_on_ets/1)
+
+  defp do_save_on_ets(%{"listing_uuid" => uuid, "suggested_listing_uuids" => uuids}) do
+    :ets.insert(:aliketeller, {uuid, uuids})
+  end
 end
--- apps/re/lib/aliketeller/server.ex
+++ apps/re/lib/aliketeller/server.ex
@@ -3,10 +3,8 @@ defmodule Re.AlikeTeller.Server do
   Module responsible for storing similar listing mapping
   """
   use GenServer
-  require Logger
-  require Mockery.Macro
 
-  @url Application.get_env(:re, :aliketeller_url, "")
+  alias Re.AlikeTeller
 
   @spec start_link :: GenServer.start_link()
   def start_link, do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
@@ -14,46 +12,15 @@ defmodule Re.AlikeTeller.Server do
   @spec init(term) :: {:ok, term}
   def init(_), do: {:ok, [], {:continue, :load_aliketeller}}
 
-  def handle_continue(:load_aliketeller, state), do: load_aliketeller(state)
-
-  def handle_cast(:load_aliketeller, state), do: load_aliketeller(state)
-
-  def handle_call(:inspect, _caller, state), do: {:reply, state, state}
-
-  defp load_aliketeller(state) do
-    create_ets_table()
-
-    with {:ok, %{status_code: 200, body: body}} <- get_payload(),
-         {:ok, payload} <- Jason.decode(body) do
-      save_on_ets(payload)
-    else
-      error -> Logger.warn("Error loading aliketeller payload: #{inspect(error)}")
-    end
+  def handle_continue(:load_aliketeller, state) do
+    AlikeTeller.load_aliketeller()
 
     {:noreply, state}
   end
 
-  defp get_payload do
-    @url
-    |> URI.parse()
-    |> http_client().get()
+  def handle_cast(:load_aliketeller, state) do
+    AlikeTeller.load_aliketeller()
+
+    {:noreply, state}
   end
-
-  defp save_on_ets(%{"data" => data}), do: Enum.each(data, &do_save_on_ets/1)
-
-  defp do_save_on_ets(%{"listing_uuid" => uuid, "suggested_listing_uuids" => uuids}) do
-    :ets.insert(:aliketeller, {uuid, uuids})
-  end
-
-  defp create_ets_table do
-    case :ets.whereis(:aliketeller) do
-      :undefined ->
-        :ets.new(:aliketeller, [:set, :protected, :named_table, read_concurrency: true])
-
-      _ ->
-        :ok
-    end
-  end
-
-  defp http_client, do: Mockery.Macro.mockable(HTTPoison)
 end
