2cf378897c9711af7207704bc99c575a68b95537
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2019-08-09 14:44:31 -0300

    style(priceteller): use sigil for multiple quoting inside string

--- apps/re_web/test/graphql/interests/price_suggestions/query_test.exs
+++ apps/re_web/test/graphql/interests/price_suggestions/query_test.exs
@@ -26,7 +26,7 @@ defmodule ReWeb.GraphQL.Interests.PriceSuggestions.QueryTest do
      admin_user: admin_user}
   end
 
-  @mock_response "{\"sale_price_rounded\":24195.0,\"sale_price\":24195.791,\"listing_price_rounded\":26279.0,\"listing_price\":26279.915,\"listing_price_error_q90_min\":25200.0,\"listing_price_error_q90_max\":28544.0,\"listing_price_per_sqr_meter\":560.0,\"listing_average_price_per_sqr_meter\":610.0}"
+  @mock_response ~s|{"sale_price_rounded":24195.0,"sale_price":24195.791,"listing_price_rounded":26279.0,"listing_price":26279.915,"listing_price_error_q90_min":25200.0,"listing_price_error_q90_max":28544.0,"listing_price_per_sqr_meter":560.0,"listing_average_price_per_sqr_meter":610.0}|
 
   describe "priceSuggestionRequest" do
     @variables %{
