d7bb68746975597f8d06055f477c6fc4e7ba93f7
Gabriela Seabra <gabriela.lima.se@gmail.com>
2019-08-12 14:08:04 -0300

    feat(salesforce) only start required services for ScheduleVisits task

--- apps/re_integrations/lib/mix/tasks/salesfoce.schedule_visits.ex
+++ apps/re_integrations/lib/mix/tasks/salesfoce.schedule_visits.ex
@@ -12,18 +12,18 @@ defmodule Mix.Tasks.ReIntegrations.Salesforce.ScheduleVisits do
 
   @shortdoc "Schedule visits"
 
-  @start_apps [
-    :timex,
-    :postgrex,
-    :ecto,
-    :ecto_sql
-  ]
+  @start_apps [:timex, :postgrex, :ecto, :ecto_sql]
+  @repos [Re.Repo, ReIntegrations.Repo]
   @timezone Application.get_env(:re_integrations, :timezone, "Etc/UTC")
 
   def run(_) do
-    Application.load(:re_integrations)
-    Enum.each(@start_apps, &Application.ensure_all_started/1)
+    start()
     Salesforce.schedule_visits(date: Timex.now(@timezone) |> Timex.shift(days: 1))
     Mix.shell().info("Routific job enqueued for monitoring.")
+  end
+
+  defp start do
+    Enum.each(@start_apps, &Application.ensure_all_started/1)
+    Enum.each(@repos, & &1.start_link(pool_size: 1))
   end
 end
