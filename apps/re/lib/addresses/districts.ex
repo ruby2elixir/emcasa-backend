a24d8743a9b38cd6c821d76bd1e91fe5aace21ec
Luis Gustavo <luigugah@gmail.com>
2019-08-13 11:18:40 -0300

    style: formating files

--- apps/re/lib/addresses/districts.ex
+++ apps/re/lib/addresses/districts.ex
@@ -1,5 +1,4 @@
 defmodule Re.Districts do
-
   @moduledoc """
   Context for handling districts
   """
@@ -17,12 +16,13 @@ defmodule Re.Districts do
   def query(query, _args), do: query
 
   def districts_by_broker_user(user_uuid) do
-    Repo.all(from(
-      d in District,
-      join: bd in BrokerDistrict,
-      where: bd.user_uuid == ^user_uuid,
-      select: d
-    ))
+    Repo.all(
+      from(
+        d in District,
+        join: bd in BrokerDistrict,
+        where: bd.user_uuid == ^user_uuid,
+        select: d
+      )
+    )
   end
-
 end
