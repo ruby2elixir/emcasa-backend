11233e39abb7f198dc5d7303e7873955db071639
pmargreff <pmargreff@gmail.com>
2019-08-14 12:02:48 -0300

    feature(shortlist): add new types to salesforce parser
    Add adapters to convert salesforce values into correct and usable
    types to plug into and external API.

--- apps/re/lib/shortlists/salesforce.opportunity.ex
+++ apps/re/lib/shortlists/salesforce.opportunity.ex
@@ -1,6 +1,6 @@
 defmodule Re.Shortlists.Salesforce.Opportunity do
   @moduledoc """
-  Module for validating salesforce opportunity entity on shortlist context
+  Module for validating and parse salesforce opportunity entity on shortlist context
   """
   use Ecto.Schema
 
@@ -11,41 +11,43 @@ defmodule Re.Shortlists.Salesforce.Opportunity do
   @primary_key {:id, :string, []}
 
   defenum(Schema,
-    infra: "Infraestrutura__c",
+    infrastructure: "Infraestrutura__c",
     type: "Tipo_do_Imovel__c",
-    rooms: "Quantidade_Minima_de_Quartos__c",
-    suites: "Quantidade_MInima_de_SuItes__c",
-    bathrooms: "Quantidade_Minima_de_Banheiros__c",
+    min_rooms: "Quantidade_Minima_de_Quartos__c",
+    min_suites: "Quantidade_MInima_de_SuItes__c",
+    min_bathrooms: "Quantidade_Minima_de_Banheiros__c",
+    min_garage_spots: "Numero_Minimo_de_Vagas__c",
+    min_area: "Area_Desejada__c",
     floor: "Andar_de_Preferencia__c",
     elevators: "Necessita_Elevador__c",
-    area: "Area_Desejada__c",
-    subway: "Proximidade_de_Metr__c",
-    garage_spots: "Numero_Minimo_de_Vagas__c",
-    neighborhood: "Bairros_de_Interesse__c",
-    price: "Valor_M_ximo_para_Compra_2__c",
-    maintenance_fee: "Valor_M_ximo_de_Condom_nio__c",
+    nearby_subway: "Proximidade_de_Metr__c",
+    neighborhoods: "Bairros_de_Interesse__c",
+    price_range: "Valor_M_ximo_para_Compra_2__c",
+    maintenance_fee_range: "Valor_M_ximo_de_Condom_nio__c",
     lobby: "Portaria_2__c"
   )
 
   schema "salesforce_opportunity" do
-    field :infra, :string
+    field :infrastructure, {:array, :string}
     field :type, :string
-    field :rooms, :string
-    field :suites, :string
-    field :bathrooms, :string
+    field :min_rooms, :integer
+    field :min_suites, :integer
+    field :min_bathrooms, :integer
+    field :min_garage_spots, :integer
+    field :min_area, :integer
     field :floor, :string
-    field :elevators, :string
-    field :area, :string
-    field :subway, :string
-    field :garage_spots, :string
-    field :neighborhood, :string
-    field :price, :string
-    field :maintenance_fee, :string
+    field :elevators, :boolean
+    field :nearby_subway, :boolean
+    field :neighborhoods, {:array, :string}
+    field :price_range, {:array, :integer}
+    field :maintenance_fee_range, {:array, :integer}
     field :lobby, :string
   end
 
-  @params ~w(infra type rooms suites bathrooms floor elevators area subway garage_spots neighborhood
-  price maintenance_fee lobby)a
+  @params ~w(infrastructure type min_rooms min_suites min_bathrooms min_garage_spots min_area floor elevators nearby_subway
+   neighborhoods price_range maintenance_fee_range lobby)a
+
+  @ignorable "Indiferente"
 
   def validate(params) do
     %__MODULE__{}
@@ -63,8 +65,93 @@ defmodule Re.Shortlists.Salesforce.Opportunity do
     |> validate()
   end
 
-  defp build_field({field, value}),
-    do: with({:ok, key} <- Schema.cast(field), do: {key, value})
+  defp build_field({"Valor_M_ximo_para_Compra_2__c" = field, value}) do
+    price_range = build_price_range(value)
+    with({:ok, key} <- Schema.cast(field), do: {key, price_range})
+  end
+
+  defp build_field({"Valor_M_ximo_de_Condom_nio__c" = field, value}) do
+    maintenance_fee_range = build_maintenance_fee_range(value)
+    with({:ok, key} <- Schema.cast(field), do: {key, maintenance_fee_range})
+  end
+
+  @multipick_field ~w(Infraestrutura__c Bairros_de_Interesse__c)
+  defp build_field({field, value}) when field in @multipick_field do
+    features =
+      value
+      |> String.split(";")
+      |> Enum.reject(&(&1 == @ignorable))
+
+    with({:ok, key} <- Schema.cast(field), do: {key, features})
+  end
+
+  defp build_field({"Area_Desejada__c" = field, value}) do
+    min_area = build_min_area(value)
+    with({:ok, key} <- Schema.cast(field), do: {key, min_area})
+  end
+
+  @optional_boolean_field ~w(Necessita_Elevador__c Proximidade_de_Metr__c)
+  defp build_field({field, value}) when field in @optional_boolean_field do
+    boolean_value =
+      case value do
+        "Sim" -> true
+        "Não" -> false
+        @ignorable -> nil
+        _ -> :i_have_no_idea_how_to_handle
+      end
+
+    with({:ok, key} <- Schema.cast(field), do: {key, boolean_value})
+  end
+
+  defp build_field({field, value}) do
+    with({:ok, key} <- Schema.cast(field), do: {key, value})
+  end
+
+  defp build_maintenance_fee_range(value) do
+    case value do
+      "Até R$500" -> [0, 500]
+      "R$500 a R$800" -> [500, 800]
+      "R$800 a R$1.000" -> [800, 1000]
+      "R$1.000 a R$1.200" -> [1_000, 1_200]
+      "R$1.200 a R$1.500" -> [1_200, 1_500]
+      "R$1.500 a R$2.000" -> [1_500, 2_000]
+      "Acima de R$2.000" -> [2_000, 20_000]
+      @ignorable -> nil
+      # not sure about it
+      _ -> :unknown
+    end
+  end
+
+  defp build_min_area(value) do
+    case value do
+      "A partir de 20m²" -> 20
+      "A partir de 60m²" -> 60
+      "A partir de 80m²" -> 80
+      "A partir de 100m²" -> 100
+      "A partir de 150m²" -> 150
+      # not sure about it
+      _ -> :unknown
+    end
+  end
+
+  defp build_price_range(value) do
+    case value do
+      "Até R$400.000" -> [0, 400_000]
+      "Até R$500.000" -> [0, 500_000]
+      "Até R$600.000" -> [0, 600_000]
+      "Até R$700.000" -> [0, 700_000]
+      "Até R$800.000" -> [0, 800_000]
+      "Até R$900.000" -> [0, 900_000]
+      "Até R$1.000.000" -> [0, 1_000_000]
+      "Até R$1.500.000" -> [0, 1_500_000]
+      "De R$500.000 a R$750.000" -> [500_000, 750_000]
+      "De R$750.000 a R$1.000.000" -> [750_000, 1_000_000]
+      "De R$1.000.000 a R$1.500.000" -> [1_000_000, 1_500_000]
+      "Acima de R$2.000.000" -> [2_000_000, 20_000_000]
+      # not sure about it
+      _ -> :unknown
+    end
+  end
 
   defp changeset(struct, params), do: cast(struct, params, @params)
 end
--- apps/re/test/shortlists/salesforce.opportunity_test.exs
+++ apps/re/test/shortlists/salesforce.opportunity_test.exs
@@ -4,38 +4,48 @@ defmodule Re.Shortlists.Salesforce.OpportunityTest do
   alias Re.Shortlists.Salesforce.Opportunity
 
   @payload %{
-    "Infraestrutura__c" => "Indiferente",
+    "Infraestrutura__c" => "Indiferente;Sacada;Churrasqueira",
     "Tipo_do_Imovel__c" => "Apartamento",
     "Quantidade_Minima_de_Quartos__c" => "1",
     "Quantidade_MInima_de_SuItes__c" => "1",
     "Quantidade_Minima_de_Banheiros__c" => "1",
+    "Numero_Minimo_de_Vagas__c" => "1",
+    "Area_Desejada__c" => "A partir de 60m²",
     "Andar_de_Preferencia__c" => "Alto",
     "Necessita_Elevador__c" => "Indiferente",
-    "Area_Desejada__c" => "A partir de 60m²",
     "Proximidade_de_Metr__c" => "Sim",
-    "Numero_Minimo_de_Vagas__c" => "1",
-    "Bairros_de_Interesse__c" => "Botafogo",
+    "Bairros_de_Interesse__c" => "Botafogo;Urca",
     "Valor_M_ximo_para_Compra_2__c" => "De R$750.000 a R$1.000.000",
     "Valor_M_ximo_de_Condom_nio__c" => "R$800 a R$1.000",
     "Portaria_2__c" => "Indiferente"
   }
 
   describe "build/1" do
+    @tag dev: true
     test "builds payload struct from salesforce response" do
       assert {:ok, %Opportunity{} = opportunity} = Opportunity.build(@payload)
-      assert opportunity.infra == @payload["Infraestrutura__c"]
+      assert opportunity.infrastructure == ["Sacada", "Churrasqueira"]
       assert opportunity.type == @payload["Tipo_do_Imovel__c"]
-      assert opportunity.rooms == @payload["Quantidade_Minima_de_Quartos__c"]
-      assert opportunity.suites == @payload["Quantidade_MInima_de_SuItes__c"]
-      assert opportunity.bathrooms == @payload["Quantidade_Minima_de_Banheiros__c"]
+
+      assert opportunity.min_rooms ==
+               @payload["Quantidade_Minima_de_Quartos__c"] |> String.to_integer()
+
+      assert opportunity.min_suites ==
+               @payload["Quantidade_MInima_de_SuItes__c"] |> String.to_integer()
+
+      assert opportunity.min_bathrooms ==
+               @payload["Quantidade_Minima_de_Banheiros__c"] |> String.to_integer()
+
+      assert opportunity.min_garage_spots ==
+               @payload["Numero_Minimo_de_Vagas__c"] |> String.to_integer()
+
+      assert opportunity.min_area == 60
       assert opportunity.floor == @payload["Andar_de_Preferencia__c"]
-      assert opportunity.elevators == @payload["Necessita_Elevador__c"]
-      assert opportunity.area == @payload["Area_Desejada__c"]
-      assert opportunity.subway == @payload["Proximidade_de_Metr__c"]
-      assert opportunity.garage_spots == @payload["Numero_Minimo_de_Vagas__c"]
-      assert opportunity.neighborhood == @payload["Bairros_de_Interesse__c"]
-      assert opportunity.price == @payload["Valor_M_ximo_para_Compra_2__c"]
-      assert opportunity.maintenance_fee == @payload["Valor_M_ximo_de_Condom_nio__c"]
+      assert opportunity.elevators == nil
+      assert opportunity.nearby_subway == true
+      assert opportunity.neighborhoods == ["Botafogo", "Urca"]
+      assert opportunity.price_range == [750_000, 1_000_000]
+      assert opportunity.maintenance_fee_range == [800, 1_000]
       assert opportunity.lobby == @payload["Portaria_2__c"]
     end
   end
