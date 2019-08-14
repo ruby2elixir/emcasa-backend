35b8998a8e7a3582b231208eb23bdfcc3bfe4fe3
Gabriela Seabra <gabriela.lima.se@gmail.com>
2019-08-14 11:18:49 -0300

    feat(salesforce) add date option to salesforce task

--- apps/re_integrations/lib/mix/tasks/salesfoce.schedule_visits.ex
+++ apps/re_integrations/lib/mix/tasks/salesfoce.schedule_visits.ex
@@ -16,9 +16,9 @@ defmodule Mix.Tasks.ReIntegrations.Salesforce.ScheduleVisits do
   @repos [Re.Repo, ReIntegrations.Repo]
   @timezone Application.get_env(:re_integrations, :timezone, "Etc/UTC")
 
-  def run(_) do
+  def run(args) do
     start()
-    date = @timezone |> Timex.now() |> Timex.shift(days: 1)
+    date = get_date(args)
     Mix.shell().info("Scheduling visits for #{date}.")
     Salesforce.schedule_visits(date: date)
     Mix.shell().info("Routific job enqueued for monitoring.")
@@ -28,4 +28,16 @@ defmodule Mix.Tasks.ReIntegrations.Salesforce.ScheduleVisits do
     Enum.each(@start_apps, &Application.ensure_all_started/1)
     Enum.each(@repos, & &1.start_link(pool_size: 1))
   end
+
+  defp tz_offset, do: @timezone |> Timex.timezone(Timex.now()) |> Timex.Timezone.total_offset()
+
+  defp get_date([date_string | _args]) do
+    with {:ok, date} <- Timex.parse(date_string, "{YYYY}-{0M}-{0D}") do
+      date
+      |> Timex.Timezone.convert(@timezone)
+      |> Timex.shift(seconds: -tz_offset())
+    end
+  end
+
+  defp get_date(_), do: @timezone |> Timex.now() |> Timex.shift(days: 1)
 end
