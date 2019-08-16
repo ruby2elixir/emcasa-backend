b0a47d04710a600a8466416ed9323d72d4dfd41f
pmargreff <pmargreff@gmail.com>
2019-08-16 15:28:47 -0300

    chore(format): add test as dir to run formatter

--- apps/re/test/accounts/users_test.exs
+++ apps/re/test/accounts/users_test.exs
@@ -11,7 +11,7 @@ defmodule Re.Accounts.UsersTest do
     test "should update a without options" do
       user = insert(:user, role: "user")
 
-      update_param = %{email: "a@a.com", name:  "Xurupita", role: "admin"}
+      update_param = %{email: "a@a.com", name: "Xurupita", role: "admin"}
       assert {:ok, user} = Users.update(user, update_param)
       assert update_param.email == user.email
       assert update_param.name == user.name
--- apps/re_integrations/.formatter.exs
+++ apps/re_integrations/.formatter.exs
@@ -1,5 +1,5 @@
 [
-  inputs: ["mix.exs", "lib/**/*.{ex,exs}", "priv/repo/migrations/*.exs"],
+  inputs: ["mix.exs", "lib/**/*.{ex,exs}", "test/**/*.{ex,exs}", "priv/repo/migrations/*.exs"],
   locals_without_parens: [
     field: 2,
     field: 3,
