1248bbf596e8e3571c220cc0ec4e59b3450eea64
pmargreff <pmargreff@gmail.com>
2019-08-16 15:22:40 -0300

    test(sholtlist): add access level tests to shortlist

--- apps/re_web/test/graphql/shortlists/query_test.exs
+++ apps/re_web/test/graphql/shortlists/query_test.exs
@@ -1,5 +1,8 @@
 defmodule ReWeb.GraphQL.Shortlists.QueryTest do
-  use ReWeb.ConnCase
+  use ReWeb.{
+    AbsintheAssertions,
+    ConnCase
+  }
 
   import Mockery
   import Re.Factory
@@ -8,21 +11,18 @@ defmodule ReWeb.GraphQL.Shortlists.QueryTest do
 
   setup %{conn: conn} do
     conn = put_req_header(conn, "accept", "application/json")
-    admin_user = insert(:user, email: "admin@email.com", role: "admin")
-    user_user = insert(:user, email: "user@email.com", role: "user")
+    admin_user = insert(:user, role: "admin")
+    user_user = insert(:user, role: "user")
 
     {:ok,
      unauthenticated_conn: conn,
-     admin_user: admin_user,
-     user_user: user_user,
      admin_conn: login_as(conn, admin_user),
      user_conn: login_as(conn, user_user)}
   end
 
   @http_client Application.get_env(:re, :http)
 
-  @tag dev: true
-  test "should return listings with relaxed filters for admin", %{admin_conn: conn} do
+  test "should return listings from shortlist for admin", %{admin_conn: conn} do
     mock(
       @http_client,
       :get,
@@ -60,6 +60,48 @@ defmodule ReWeb.GraphQL.Shortlists.QueryTest do
     conn = post(conn, "/graphql_api", AbsintheHelpers.query_wrapper(query, variables))
 
     assert [%{"id" => to_string(listing_id)}] ==
+             json_response(conn, 200)["data"]["shortlistListings"]
+  end
+
+  test "should return a forbidden for commom user", %{user_conn: conn} do
+    variables = %{
+      "opportunityId" => "0x01"
+    }
+
+    query = """
+      query ShortlistListings ($opportunityId: String) {
+        shortlistListings (opportunityId: $opportunityId) {
+          id
+        }
+      }
+    """
+
+    conn = post(conn, "/graphql_api", AbsintheHelpers.query_wrapper(query, variables))
+
+    assert_forbidden_response(json_response(conn, 200))
+
+    assert nil ==
+             json_response(conn, 200)["data"]["shortlistListings"]
+  end
+
+  test "should return a unauthorize for unauthenticated user", %{conn: conn} do
+    variables = %{
+      "opportunityId" => "0x01"
+    }
+
+    query = """
+      query ShortlistListings ($opportunityId: String) {
+        shortlistListings (opportunityId: $opportunityId) {
+          id
+        }
+      }
+    """
+
+    conn = post(conn, "/graphql_api", AbsintheHelpers.query_wrapper(query, variables))
+
+    assert_unauthorized_response(json_response(conn, 200))
+
+    assert nil ==
              json_response(conn, 200)["data"]["shortlistListings"]
   end
 end
