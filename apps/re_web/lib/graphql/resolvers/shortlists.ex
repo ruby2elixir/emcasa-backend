5b43472c2768c54c45825869eafd9bad8535933d
pmargreff <pmargreff@gmail.com>
2019-08-22 10:03:43 -0300

    refactor(shortlist): alter authorization name to resource

--- apps/re_web/lib/graphql/resolvers/shortlists.ex
+++ apps/re_web/lib/graphql/resolvers/shortlists.ex
@@ -5,7 +5,7 @@ defmodule ReWeb.Resolvers.Shortlists do
   alias Re.Shortlists
 
   def index(%{opportunity_id: opportunity_id}, %{context: %{current_user: current_user}}) do
-    case Bodyguard.permit(Shortlists, :related_listings, current_user, %{}) do
+    case Bodyguard.permit(Shortlists, :shortlist_listings, current_user, %{}) do
       :ok -> {:ok, Shortlists.generate_shortlist_from_salesforce_opportunity(opportunity_id)}
       error -> error
     end
