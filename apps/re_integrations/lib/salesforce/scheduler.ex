28c7020683de927eff68dba8dcaaaebd44a028ee
Gabriela Seabra <gabriela.lima.se@gmail.com>
2019-08-28 16:02:58 -0300

    refac(salesforce) refactor scheduler function

--- apps/re_integrations/lib/salesforce/scheduler.ex
+++ apps/re_integrations/lib/salesforce/scheduler.ex
@@ -7,24 +7,17 @@ defmodule ReIntegrations.Salesforce.Scheduler do
 
   @timezone Application.get_env(:re_integrations, :timezone, "Etc/UTC")
 
-  def schedule_daily_visits(nil), do: @timezone |> Timex.now() |> schedule_daily_visits()
+  def schedule_daily_visits(), do: @timezone |> Timex.now() |> schedule_daily_visits()
 
   def schedule_daily_visits(%DateTime{} = today) do
     today
     |> Timex.weekday()
-    |> case do
-      7 ->
-        []
-
-      6 ->
-        []
-
-      5 ->
-        [Timex.shift(today, days: 1), Timex.shift(today, days: 3)]
-
-      _ ->
-        [Timex.shift(today, days: 1)]
-    end
-    |> Enum.map(&Salesforce.schedule_visits(date: &1))
+    |> dates_to_schedule_on_weekday()
+    |> Enum.map(&Salesforce.schedule_visits(date: Timex.shift(today, &1)))
   end
+
+  defp dates_to_schedule_on_weekday(7), do: []
+  defp dates_to_schedule_on_weekday(6), do: []
+  defp dates_to_schedule_on_weekday(5), do: [[days: 1], [days: 3]]
+  defp dates_to_schedule_on_weekday(_), do: [[days: 1]]
 end
